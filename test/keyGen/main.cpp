54b9ff4387379b3f1b3b26126ff8b1d772af5248
rossi.kwak <rossi.kwak@gmail.com>
2017-04-21 13:13:54 +0900

    softhsm2라이브러리 로드 구현

--- test/keyGen/main.cpp
+++ test/keyGen/main.cpp
@@ -1,9 +1,19 @@
 #include <iostream>
+#include "library.h"
 
 using namespace std;
 
 int main(int argc, const char* argv[])
 {
+	void* module;
+	CK_FUNCTION_LIST_PTR p11 = NULL;
+	if (loadLib(&module, &p11) == -1) {
+		cout << "ERROR: loadLib" << endl;
+		return -1;
+	}
+	cout << "loadLib ok" << endl;
+
+	unloadLib(module);
 	cout << "end" << endl;
 	return 0;
 }
