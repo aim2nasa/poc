2f56030fd54628b55f1b07827e11d117449cc652
rossi.kwak <rossi.kwak@gmail.com>
2017-05-08 18:04:22 +0900

    * hKey값 검사 테스트 추가

--- test/unitTest/aesWrapUnwrapTest.cpp
+++ test/unitTest/aesWrapUnwrapTest.cpp
@@ -59,4 +59,5 @@ TEST_F(AesWrapUnwrapTest, aesKeyGen)
 	EXPECT_NE(_hSession, CK_INVALID_HANDLE);
 	CK_OBJECT_HANDLE hKey = CK_INVALID_HANDLE;
 	EXPECT_EQ(generateAesKey(IN_SESSION, IS_PUBLIC, 32, "testAesKey", hKey), CKR_OK);
+	EXPECT_NE(hKey, CK_INVALID_HANDLE);
 }
