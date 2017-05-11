a09b1b371bb2eb646ee25f5ae0f21dce41c2e85c
rossi.kwak <rossi.kwak@gmail.com>
2017-05-11 15:30:57 +0900

    * 테스트케이스 이름 변경 (aesWrap -> aesWrapUnwrap) * aesWrapUnwrap 테스트케이스 완성  hKey는 wrap또는 unwrap할때 사용하는 암호화키라고 보면 된다  hSecret은 C_GenerateRandom을 사용하여 keyPrt버퍼를 채우고 CKA_VALUE로 값을 채운 객체이다. * 테스트 케이스 설명  1. hSecret을 hKey로 wrap하여 wrappedPtr을 얻는다. 이는 keyPtr버퍼가 hKey로 암호화된 것이라 보면 된다.  2. wrappedPtr은 hKey로 unwrap하여 hNew객체를 얻고 이 객체에서 속성값을 읽어와 다시 복호화된 원래의 keyPtr버퍼를 갖고옴을 확인한다.

--- test/unitTest/aesWrapUnwrapTest.cpp
+++ test/unitTest/aesWrapUnwrapTest.cpp
@@ -62,8 +62,9 @@ TEST_F(AesWrapUnwrapTest, aesKeyGen)
 	EXPECT_NE(hKey, CK_INVALID_HANDLE);
 }
 
-TEST_F(AesWrapUnwrapTest, aesWrap)
+TEST_F(AesWrapUnwrapTest, aesWrapUnwrap)
 {
+	//hKey, Wrapping에 사용할 암호화 key준비
 	CK_BBOOL bToken = CK_FALSE;		//IN_SESSION
 	CK_BBOOL bPrivate = CK_FALSE;	//IS_PUBLIC
 
@@ -72,6 +73,7 @@ TEST_F(AesWrapUnwrapTest, aesWrap)
 	EXPECT_EQ(generateAesKey(bToken, bPrivate, 32, "testAesKey", hKey), CKR_OK);
 	EXPECT_NE(hKey, CK_INVALID_HANDLE);
 
+	//hSecret, Wrapping의 대상이 되는 객체 준비 (키는 C_GenerateRandom을 사용해서 생성한다.)
 	CK_MECHANISM_TYPE mechanismType = CKM_AES_KEY_WRAP;
 	CK_MECHANISM mechanism = { mechanismType, NULL_PTR, 0 };
 	CK_BBOOL bFalse = CK_FALSE;
@@ -90,18 +92,51 @@ TEST_F(AesWrapUnwrapTest, aesWrap)
 		{ CKA_VALUE, keyPtr, keyLen }
 	};
 
+	EXPECT_EQ(C_GenerateRandom(_hSession, keyPtr, keyLen), CKR_OK);
 	CK_OBJECT_HANDLE hSecret = CK_INVALID_HANDLE;
 	EXPECT_EQ(C_CreateObject(_hSession, attribs, sizeof(attribs) / sizeof(CK_ATTRIBUTE), &hSecret), CKR_OK);
 	EXPECT_NE(hSecret, CK_INVALID_HANDLE);
 
+	//hSecret을 hKey를 사용하여 wrapping하여 암호화된 버퍼 wrappedPtr을 얻는다, (wrappedPtr은 keyPtr이 hKey의 키로 암호화된 버퍼다)
 	CK_BYTE_PTR wrappedPtr = NULL_PTR;
 	CK_ULONG wrappedLen = 0UL;
+	CK_ULONG rndKeyLen = keyLen;
 	EXPECT_EQ(C_WrapKey(_hSession, &mechanism, hKey, hSecret, NULL, &wrappedLen), CKR_OK);
-	EXPECT_NE(wrappedLen, 0);
+	EXPECT_EQ(wrappedLen, rndKeyLen + 8);
 
 	wrappedPtr = (CK_BYTE_PTR)malloc(wrappedLen);
 	EXPECT_EQ(C_WrapKey(_hSession, &mechanism, hKey, hSecret, wrappedPtr, &wrappedLen), CKR_OK);
-	EXPECT_NE(wrappedLen, 0);
+	EXPECT_EQ(wrappedLen, rndKeyLen + 8);
+
+	//암호화된 wrappedPtr을 풀기 위하여 hKey를 사용하여 unwrap하고 그 결과를 담은 hNew객체를 리턴받는다
+	CK_ATTRIBUTE nattribs[] = {
+		{ CKA_CLASS, &secretClass, sizeof(secretClass) },
+		{ CKA_KEY_TYPE, &genKeyType, sizeof(genKeyType) },
+		{ CKA_TOKEN, &bFalse, sizeof(bFalse) },
+		{ CKA_PRIVATE, &bFalse, sizeof(bTrue) },
+		{ CKA_ENCRYPT, &bFalse, sizeof(bFalse) },
+		{ CKA_DECRYPT, &bTrue, sizeof(bTrue) },
+		{ CKA_SIGN, &bFalse, sizeof(bFalse) },
+		{ CKA_VERIFY, &bTrue, sizeof(bTrue) },
+		{ CKA_EXTRACTABLE, &bTrue, sizeof(bTrue) }
+	};
+	CK_OBJECT_HANDLE hNew = CK_INVALID_HANDLE;
+	EXPECT_EQ(C_UnwrapKey(_hSession, &mechanism, hKey, wrappedPtr, wrappedLen, nattribs, sizeof(nattribs) / sizeof(CK_ATTRIBUTE), &hNew),CKR_OK);
+	EXPECT_NE(hNew , CK_INVALID_HANDLE);
+
+	CK_ATTRIBUTE keyAttribs[] = {
+		{ CKA_VALUE, NULL_PTR, 0 }
+	};
+
+	//hNew객체의 속성, 키 버퍼값과 길이 값을 확인하여 원래의 keyPtr과 동일한 키가 hNew에 들어있음을 확인한다.
+	EXPECT_EQ(C_GetAttributeValue(_hSession, hNew, keyAttribs, 1),CKR_OK);
+	EXPECT_EQ(keyLen,keyAttribs[0].ulValueLen);
+
+	keyAttribs[0].pValue = (CK_BYTE_PTR)malloc(keyAttribs[0].ulValueLen);
+	EXPECT_EQ(C_GetAttributeValue(_hSession, hNew, keyAttribs, 1), CKR_OK);
+	EXPECT_EQ(keyLen, keyAttribs[0].ulValueLen);
+	EXPECT_EQ(memcmp(keyPtr,keyAttribs[0].pValue,keyLen),0);
+	free(keyAttribs[0].pValue);
 
 	free(wrappedPtr);
 }
