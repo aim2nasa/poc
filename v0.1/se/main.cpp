e87388bb652e71add6bab4625db577c9a461f6d7
rossi.kwak <rossi.kwak@gmail.com>
2017-05-16 17:59:17 +0900

    * http://jarvis.iptime.org:9090/svn/aceStudy의 reactorCli를 참고하여 se(secure element) 초안 구현  se는 gateway에 접속하여 필요한 키들을 할당 받는다

---
+++ v0.1/se/main.cpp
@@ -0,0 +1,62 @@
+#include <iostream>
+#include "ace/SOCK_Connector.h" 
+#include "ace/INET_Addr.h" 
+#include "ace/Log_Msg.h" 
+#include "ace/OS_NS_stdio.h" 
+#include "ace/OS_NS_string.h" 
+#include "ace/OS_NS_unistd.h"
+#include "ace/OS_NS_stdlib.h"
+
+#define SIZE_BUF 256
+
+static char* SERVER_HOST = "127.0.0.1";
+static u_short SERVER_PORT = 98765;
+
+int main(int argc, char *argv[])
+{
+	const char *server_host = argc > 1 ? argv[1] : SERVER_HOST;
+	u_short server_port = argc > 2 ? ACE_OS::atoi(argv[2]) : SERVER_PORT;
+	ACE_DEBUG((LM_INFO, "(%P|%t) server info(addr:%s,port:%d)\n", server_host, server_port));
+
+	ACE_SOCK_Stream client_stream;
+	ACE_INET_Addr remote_addr(server_port, server_host);
+	ACE_SOCK_Connector connector;
+
+	ACE_DEBUG((LM_DEBUG, "(%P|%t) Starting connect to %s: %d \n", remote_addr.get_host_name(), remote_addr.get_port_number()));
+	if (connector.connect(client_stream, remote_addr) == -1)
+		ACE_ERROR_RETURN((LM_ERROR, "(%P|%t) %p \n", "connection failed"), -1);
+	else
+		ACE_DEBUG((LM_DEBUG, "(%P|%t) connected to %s \n", remote_addr.get_host_name()));
+
+	int nRtn = 0;
+	char buffer[SIZE_BUF];
+	std::cout << "press q and enter to finish" << std::endl;
+	while (true){
+		std::cout << ": ";
+		fgets(buffer, sizeof(buffer), stdin);
+
+		if (ACE_OS::strcmp(buffer, "q\n") == 0)
+			break;
+
+		if ((nRtn = client_stream.send_n(buffer, SIZE_BUF)) == -1) {
+			ACE_DEBUG((LM_DEBUG, "(%P|%t) Error send_n(%d)\n", nRtn));
+			break;
+		}
+
+		ACE_DEBUG((LM_DEBUG, "(%P|%t) %dbytes sent\n", nRtn));
+
+		// recv
+		char recv_buff[SIZE_BUF] = { 0 };
+		if ((nRtn = client_stream.recv_n(recv_buff, sizeof(recv_buff))) == -1) {
+			ACE_ERROR((LM_ERROR, "(%P|%t) Error recv_n(%d)\n", nRtn));
+			break;
+		}
+		else
+			ACE_DEBUG((LM_DEBUG, "(%P|%t) %dbytes received:%s\n", nRtn, recv_buff));
+	}
+
+	if (client_stream.close() == -1)
+		ACE_ERROR_RETURN((LM_ERROR, "(%P|%t) %p \n", "close"), -1);
+
+	return 0;
+}
