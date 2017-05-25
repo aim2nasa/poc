a8687768e51765f22b21b9a21896471f04866231
rossi.kwak <rossi.kwak@gmail.com>
2017-05-25 16:33:03 +0900

    CGroup에 hGroup_,hTag_멤버 추가

--- v0.1/gateway/CGroup.h
+++ v0.1/gateway/CGroup.h
@@ -5,8 +5,12 @@
 
 class CGroup{
 public:
+	CGroup() :hGroup_(CK_INVALID_HANDLE), hTag_(CK_INVALID_HANDLE){}
+
 	std::string groupName_;
 	std::list<unsigned int> cidList_;
+	CK_OBJECT_HANDLE hGroup_;
+	CK_OBJECT_HANDLE hTag_;
 };
 
 #endif
