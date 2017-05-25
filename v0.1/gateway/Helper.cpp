df4db12498e660802fa29de915a625b566abdee3
rossi.kwak <rossi.kwak@gmail.com>
2017-05-25 17:03:16 +0900

    aesDerive , deriveGroup 함수 포팅 (from test/keyGen 프로젝트)

--- v0.1/gateway/Helper.cpp
+++ v0.1/gateway/Helper.cpp
@@ -74,3 +74,66 @@ int gatewayKey(CToken &token, CK_ULONG keySize, CK_OBJECT_HANDLE &hGw)
 	}
 	ACE_RETURN(0);
 }
+
+int aesDerive(CK_SESSION_HANDLE hSession, CK_OBJECT_HANDLE hKey, CK_OBJECT_HANDLE &hDerive, CK_MECHANISM_TYPE mechType, CK_BYTE *data, CK_LONG dataSize, CK_CHAR_PTR iv)
+{
+	CK_RV rv;
+	CK_MECHANISM mechanism = { mechType, NULL_PTR, 0 };
+	CK_KEY_DERIVATION_STRING_DATA param1;
+	CK_AES_CBC_ENCRYPT_DATA_PARAMS param3;
+
+	switch (mechType)
+	{
+	case CKM_AES_ECB_ENCRYPT_DATA:
+		param1.pData = data;
+		param1.ulLen = dataSize;
+		mechanism.pParameter = &param1;
+		mechanism.ulParameterLen = sizeof(param1);
+		break;
+	case CKM_AES_CBC_ENCRYPT_DATA:
+		ACE_ASSERT(iv);
+		memcpy(param3.iv, iv, 16);
+		param3.pData = data;
+		param3.length = dataSize;
+		mechanism.pParameter = &param3;
+		mechanism.ulParameterLen = sizeof(param3);
+		break;
+	default:
+		ACE_RETURN(-1);		//Invalid mechanism
+	}
+
+	CK_KEY_TYPE keyType = CKK_AES;
+	CK_ULONG secLen = dataSize;	//8*32 = 256bit
+
+	CK_OBJECT_CLASS keyClass = CKO_SECRET_KEY;
+	CK_BBOOL bFalse = CK_FALSE;
+	CK_BBOOL bTrue = CK_TRUE;
+	CK_ATTRIBUTE keyAttribs[] = {
+		{ CKA_CLASS, &keyClass, sizeof(keyClass) },
+		{ CKA_KEY_TYPE, &keyType, sizeof(keyType) },
+		{ CKA_PRIVATE, &bFalse, sizeof(bFalse) },
+		{ CKA_ENCRYPT, &bTrue, sizeof(bTrue) },
+		{ CKA_DECRYPT, &bTrue, sizeof(bTrue) },
+		{ CKA_DERIVE, &bTrue, sizeof(bTrue) },
+		{ CKA_SENSITIVE, &bFalse, sizeof(bFalse) },
+		{ CKA_EXTRACTABLE, &bTrue, sizeof(bTrue) },
+		{ CKA_VALUE_LEN, &secLen, sizeof(secLen) }
+	};
+
+	hDerive = CK_INVALID_HANDLE;
+	rv = C_DeriveKey(hSession, &mechanism, hKey, keyAttribs, sizeof(keyAttribs) / sizeof(CK_ATTRIBUTE), &hDerive);
+	if (rv != CKR_OK) ACE_RETURN(-3);
+
+	ACE_RETURN(0);
+}
+
+//Derive Group
+int deriveGroup(CK_SESSION_HANDLE hSession, CK_OBJECT_HANDLE &hGroup, CK_OBJECT_HANDLE hParent, CK_BYTE_PTR data, CK_ULONG dataSize)
+{
+	hGroup = CK_INVALID_HANDLE;
+	int nRtn;
+	if ((nRtn = aesDerive(hSession, hParent, hGroup, CKM_AES_ECB_ENCRYPT_DATA, data, dataSize)) != 0)
+		ACE_ERROR_RETURN((LM_ERROR, ACE_TEXT("aesDerive failed:0x%x\n"), nRtn), -1);
+
+	ACE_RETURN(0);
+}
--- v0.1/gateway/Helper.h
+++ v0.1/gateway/Helper.h
@@ -6,5 +6,7 @@ class CToken;
 
 int prepareSession(CToken &token, const char *label, const char *soPin, const char *userPin);
 int gatewayKey(CToken &token, CK_ULONG keySize, CK_OBJECT_HANDLE &hGw);
+int aesDerive(CK_SESSION_HANDLE hSession, CK_OBJECT_HANDLE hKey, CK_OBJECT_HANDLE &hDerive, CK_MECHANISM_TYPE mechType, CK_BYTE *data, CK_LONG dataSize, CK_CHAR_PTR iv = NULL);
+int deriveGroup(CK_SESSION_HANDLE hSession, CK_OBJECT_HANDLE &hGroup, CK_OBJECT_HANDLE hParent, CK_BYTE_PTR data, CK_ULONG dataSize);
 
 #endif
