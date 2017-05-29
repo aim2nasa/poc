aaa90416fe8bef7f1400b1990ea7723dc20eaea0
rossi.kwak <rossi.kwak@gmail.com>
2017-05-29 16:21:55 +0900

    SE의 멤버로 tagKey_와 seKey_를 정의하고 이를 조작할 수 있는 함수 정의

--- v0.1/gateway/CSe.cpp
+++ v0.1/gateway/CSe.cpp
@@ -1,8 +1,31 @@
 #include "CSe.h"
 #include "ace\OS_NS_string.h"
+#include "ace\Assert.h"
 
 CSe::CSe()
 :h_(CK_INVALID_HANDLE), cid_(-1)
 {
+	ACE_OS::memset(tagKey_, 0, AES_KEY_SIZE);
+}
 
+void CSe::tagKey(CK_BYTE_PTR tKey, CK_ULONG size)
+{
+	ACE_ASSERT(size == AES_KEY_SIZE);
+	ACE_OS::memcpy(tagKey_, tKey, size);
+}
+
+CK_BYTE_PTR CSe::tagKey()
+{
+	return tagKey_;
+}
+
+void CSe::seKey(CK_BYTE_PTR seKey, CK_ULONG size)
+{
+	ACE_ASSERT(size == AES_KEY_SIZE);
+	ACE_OS::memcpy(seKey_, seKey, size);
+}
+
+CK_BYTE_PTR CSe::seKey()
+{
+	return seKey_;
 }
--- v0.1/gateway/CSe.h
+++ v0.1/gateway/CSe.h
@@ -8,8 +8,17 @@ class CSe{
 public:
 	CSe();
 
+	void tagKey(CK_BYTE_PTR tKey, CK_ULONG size);
+	CK_BYTE_PTR tagKey();
+	void seKey(CK_BYTE_PTR seKey, CK_ULONG size);
+	CK_BYTE_PTR seKey();
+
 	unsigned int cid_;
 	CK_OBJECT_HANDLE h_;
+
+private:
+	CK_BYTE tagKey_[AES_KEY_SIZE];
+	CK_BYTE seKey_[AES_KEY_SIZE];
 };
 
 #endif
