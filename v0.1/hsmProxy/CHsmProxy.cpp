ec225e8515ad5152c3714541b295d3717d866cc9
rossi.kwak <rossi.kwak@gmail.com>
2017-05-30 19:23:46 +0900

    * CHsmProxy init를 구현 (초기화후에 토큰에 로그인까지만 하는 함수) * softhsm2라이브러리를 링크할 수 있도록 프로젝트 설정

--- v0.1/hsmProxy/CHsmProxy.cpp
+++ v0.1/hsmProxy/CHsmProxy.cpp
@@ -1,6 +1,32 @@
 #include "CHsmProxy.h"
+#include "CToken.h"
 
 CHsmProxy::CHsmProxy()
+:token_(NULL)
 {
+	token_ = new CToken();
+}
 
+CHsmProxy::~CHsmProxy()
+{
+	delete token_;
+}
+
+int CHsmProxy::init(const char *soPin, const char *userPin)
+{
+	if (token_->initialize() != 0) {
+		sprintf_s(message_, MAX_ERR_MSG, "token initialize error");
+		return -1;
+	}
+
+	if (token_->openSession() != 0) {
+		sprintf_s(message_, MAX_ERR_MSG, "token openSession error");
+		return -1;
+	}
+
+	if (token_->login(CKU_USER, userPin, (CK_ULONG)strlen(userPin)) != 0) {
+		sprintf_s(message_, MAX_ERR_MSG, "token user login error userPin:%s", userPin);
+		return -1;
+	}
+	return 0;
 }
--- v0.1/hsmProxy/CHsmProxy.h
+++ v0.1/hsmProxy/CHsmProxy.h
@@ -1,9 +1,20 @@
 #ifndef __CHSMPROXY_H__
 #define __CHSMPROXY_H__
 
+#define MAX_ERR_MSG 256
+
+class CToken;
+
 class __declspec(dllexport) CHsmProxy{
 public:
 	CHsmProxy();
+	virtual ~CHsmProxy();
+
+	int init(const char *soPin, const char *userPin);
+
+	char message_[MAX_ERR_MSG];
+private:
+	CToken *token_;
 };
 
 #endif
