f3411d34f4d1f4a2c0067e65239d3cc37e3f7712
rossi.kwak <rossi.kwak@gmail.com>
2017-05-31 17:47:10 +0900

    * findKey 함수를 정의, 이 함수를 통해서 Tag키와 Se키를 얻어올 수 있다. * findKey함수를 테스트하는 테스트 케이스 완성

--- v0.1/hsmProxy/CHsmProxy.cpp
+++ v0.1/hsmProxy/CHsmProxy.cpp
@@ -1,5 +1,6 @@
 #include "CHsmProxy.h"
 #include "CToken.h"
+#include "protocol.h"
 #include <assert.h>
 
 CHsmProxy::CHsmProxy()
@@ -32,5 +33,21 @@ int CHsmProxy::init(const char *soPin, const char *userPin)
 		sprintf_s(message_, MAX_ERR_MSG, "token user login error userPin:%s", userPin);
 		return -4;
 	}
+	return 0;
+}
+
+int CHsmProxy::findKey(const char *label, unsigned int labelSize, unsigned long &hKey)
+{
+	CK_ATTRIBUTE attribs[] = { { CKA_LABEL, (CK_UTF8CHAR_PTR)label, labelSize } };
+
+	if(C_FindObjectsInit(token_->session(), &attribs[0], 1)!=CKR_OK) return -1;
+
+	CK_SESSION_HANDLE hK = CK_INVALID_HANDLE;
+	CK_ULONG ulObjectCount = 0;
+	if (C_FindObjects(token_->session(), &hK, 1, &ulObjectCount) != CKR_OK) return -2;
+	if (ulObjectCount > 1) return -3;	//하나의 키만을 찾게 된다는 전제를 하고 있음, 따라서 하나 이상의 경우에는 오류 처리
+
+	hKey = (unsigned long)hK;
+	C_FindObjectsFinal(token_->session());
 	return 0;
 }
--- v0.1/unitTest/hsmTest.cpp
+++ v0.1/unitTest/hsmTest.cpp
@@ -1,9 +1,19 @@
 #include <gtest/gtest.h>
 #include "CHsmProxy.h"
+#include "protocol.h"
 
 TEST(HsmTest, simple)
 {
 	EXPECT_NE(1, 2);
 	CHsmProxy p;
 	EXPECT_EQ(p.init("123456", "1234"),0);
+
+	//주의 sizeof를 하면 NULL까지 포함해서 크기가 나온다 따라서 -1을 해줘야 함
+	unsigned long hTagKey, hSeKey;
+	hTagKey=hSeKey=0;	//CK_INVALID_HANDLE = 0
+
+	EXPECT_EQ(p.findKey(TAG_KEY_LABEL, sizeof(TAG_KEY_LABEL)-1,hTagKey),0);
+	EXPECT_NE(hTagKey, 0);
+	EXPECT_EQ(p.findKey(SE_KEY_LABEL, sizeof(SE_KEY_LABEL)-1, hSeKey), 0);
+	EXPECT_NE(hSeKey, 0);
 }
