e3f83f757634cf7ab8c19f82179c62e10a71dd7d
rossi.kwak <rossi.kwak@gmail.com>
2017-07-05 17:17:54 +0900

    StudyOpenSSL프로젝트의 SslServer프로젝트에서 서버 인증서(rootcert.pem)와 개인키(rootkey.pem) 파일을 복사해와 본 프로젝트에 적용 (http://jarvis.iptime.org:8888/redmine/projects/ubiatc/repository/studyopenssl/revisions/efe915365ac8af3ac697deae7a95c303caed4eaf)

--- test/SSL_Server/main.cpp
+++ test/SSL_Server/main.cpp
@@ -6,6 +6,10 @@ int ACE_TMAIN(int argc, ACE_TCHAR *argv[])
 {
 	ACE_SSL_Context *context = ACE_SSL_Context::instance();
 
+	int nRtn;
+	nRtn = context->certificate("./rootcert.pem", SSL_FILETYPE_PEM);
+	nRtn = context->private_key("./rootkey.pem", SSL_FILETYPE_PEM);
+
 	ACE_DEBUG((LM_INFO, ACE_TEXT("(%P|%t) SSL server end\n")));
 	ACE_RETURN(0);
 }
