a7595159c5af92786d2ef10e34d832c307f4a37c
rossi.kwak <rossi.kwak@gmail.com>
2017-07-05 16:45:25 +0900

    ACE_SSL_Context를 코드에 넣고 win32,x64에서 모두 컴파일 성공이 되도록 프로젝트 설정 변경 (동작 확인함) OpenSSL 위치는 SSL_ROOT환경 변수로 지정 빌드후 ACE,ACE_SSL라이브러리 복사 release모드에서 4996 컴파일 에러 무시 설정

--- test/SSL_Server/main.cpp
+++ test/SSL_Server/main.cpp
@@ -1,8 +1,11 @@
 #include <ace/OS.h>
 #include <ace/Log_Msg.h>
+#include <ace/SSL/SSL_Context.h>
 
 int ACE_TMAIN(int argc, ACE_TCHAR *argv[])
 {
+	ACE_SSL_Context *context = ACE_SSL_Context::instance();
+
 	ACE_DEBUG((LM_INFO, ACE_TEXT("(%P|%t) SSL server end\n")));
 	ACE_RETURN(0);
 }
