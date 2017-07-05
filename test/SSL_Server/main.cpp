9184ba9a31d9d1769457daaedf2571fdcaf74cf7
rossi.kwak <rossi.kwak@gmail.com>
2017-07-05 18:42:13 +0900

    인증서와 개인키 검증 로직 추가

--- test/SSL_Server/main.cpp
+++ test/SSL_Server/main.cpp
@@ -9,6 +9,10 @@ int ACE_TMAIN(int argc, ACE_TCHAR *argv[])
 	context->certificate("./rootcert.pem", SSL_FILETYPE_PEM);
 	context->private_key("./rootkey.pem", SSL_FILETYPE_PEM);
 
+	// 읽은 인증서와 개인키가 맞는지 확인 한다.
+	if(context->verify_private_key()!=0)
+		ACE_ERROR_RETURN((LM_ERROR, ACE_TEXT("인증서와 개인키가 맞지 않습니다.\n")), -1);
+
 	ACE_DEBUG((LM_INFO, ACE_TEXT("(%P|%t) SSL server end\n")));
 	ACE_RETURN(0);
 }
