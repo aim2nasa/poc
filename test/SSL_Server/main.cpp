51438a5123798d84b9a52248724ef1406ae84399
rossi.kwak <rossi.kwak@gmail.com>
2017-07-05 17:24:35 +0900

    리턴값 삭제

--- test/SSL_Server/main.cpp
+++ test/SSL_Server/main.cpp
@@ -6,9 +6,8 @@ int ACE_TMAIN(int argc, ACE_TCHAR *argv[])
 {
 	ACE_SSL_Context *context = ACE_SSL_Context::instance();
 
-	int nRtn;
-	nRtn = context->certificate("./rootcert.pem", SSL_FILETYPE_PEM);
-	nRtn = context->private_key("./rootkey.pem", SSL_FILETYPE_PEM);
+	context->certificate("./rootcert.pem", SSL_FILETYPE_PEM);
+	context->private_key("./rootkey.pem", SSL_FILETYPE_PEM);
 
 	ACE_DEBUG((LM_INFO, ACE_TEXT("(%P|%t) SSL server end\n")));
 	ACE_RETURN(0);
