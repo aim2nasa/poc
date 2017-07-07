3db7e929578a45ee0d5e2b4eecce0a2e3d13b870
rossi.kwak <rossi.kwak@gmail.com>
2017-07-07 15:28:41 +0900

    http://jarvis.iptime.org:9090/svn/aceStudy의 aceStudy_v12솔루션내 reactorCli프로젝트의 서버와 스트림을 주고받는 구현 복사하여 적용함
    테스트 OK
    SSL_Server와 SSL_Client가 상호 연결하여 스트림을 주고받는 테스트 성공함

--- test/SSL_Client/main.cpp
+++ test/SSL_Client/main.cpp
@@ -2,8 +2,10 @@
 #include <ace/Log_Msg.h>
 #include <ace/INET_Addr.h>
 #include <ace/SSL/SSL_SOCK_Connector.h>
+#include <iostream>
 
 #define TARGET_PORT 9088
+#define SIZE_BUF	256
 
 static char* SERVER_HOST = "127.0.0.1";
 static u_short SERVER_PORT = TARGET_PORT;
@@ -20,6 +22,36 @@ int ACE_TMAIN(int argc, ACE_TCHAR *argv[])
 		ACE_ERROR_RETURN((LM_ERROR,ACE_TEXT("(%P|%t) %p\n"),ACE_TEXT("connection failed")),-1);
 	else
 		ACE_DEBUG((LM_DEBUG,ACE_TEXT("(%P|%t) connected to %C at port %d\n"),remote_addr.get_host_name(),remote_addr.get_port_number()));
+
+	int nRtn = 0;
+	char buffer[SIZE_BUF];
+	std::cout << "press q and enter to finish" << std::endl;
+	while (true){
+		std::cout << ": ";
+		fgets(buffer, sizeof(buffer), stdin);
+
+		if (ACE_OS::strcmp(buffer, "q\n") == 0)
+			break;
+
+		if ((nRtn = cli_stream.send_n(buffer, SIZE_BUF)) == -1) {
+			ACE_DEBUG((LM_DEBUG, "(%P|%t) Error send_n(%d)\n", nRtn));
+			break;
+		}
+
+		ACE_DEBUG((LM_DEBUG, "(%P|%t) %dbytes sent\n", nRtn));
+
+		// recv
+		char recv_buff[SIZE_BUF] = { 0 };
+		if ((nRtn = cli_stream.recv_n(recv_buff, sizeof(recv_buff))) == -1) {
+			ACE_ERROR((LM_ERROR, "(%P|%t) Error recv_n(%d)\n", nRtn));
+			break;
+		}
+		else
+			ACE_DEBUG((LM_DEBUG, "(%P|%t) %dbytes received:%s\n", nRtn, recv_buff));
+	}
+
+	if (cli_stream.close() == -1)
+		ACE_ERROR_RETURN((LM_ERROR, "(%P|%t) %p \n", "close"), -1);
 
 	ACE_DEBUG((LM_INFO, ACE_TEXT("(%P|%t) SSL client end\n")));
 	ACE_RETURN(0);
