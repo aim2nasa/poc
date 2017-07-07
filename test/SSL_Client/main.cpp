c36ca8988cad9e5e505530b361e5e5515ee36b6c
rossi.kwak <rossi.kwak@gmail.com>
2017-07-07 15:12:45 +0900

    SSL라이브러리 추가 ACE_INET_Addr 및 ACE_SSL_SOCK_Connector 추가

--- test/SSL_Client/main.cpp
+++ test/SSL_Client/main.cpp
@@ -1,8 +1,20 @@
 #include <ace/OS.h>
 #include <ace/Log_Msg.h>
+#include <ace/INET_Addr.h>
+#include <ace/SSL/SSL_SOCK_Connector.h>
+
+#define TARGET_PORT 9088
+
+static char* SERVER_HOST = "127.0.0.1";
+static u_short SERVER_PORT = TARGET_PORT;
 
 int ACE_TMAIN(int argc, ACE_TCHAR *argv[])
 {
+	const char *server_host = SERVER_HOST;
+	u_short server_port = SERVER_PORT;
+	ACE_INET_Addr remote_addr(server_port, server_host);
+	ACE_SSL_SOCK_Connector connector;
+
 	ACE_DEBUG((LM_INFO, ACE_TEXT("(%P|%t) SSL client end\n")));
 	ACE_RETURN(0);
 }
