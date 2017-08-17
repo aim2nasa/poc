1ce3c6b92accb67b20c45c0cfcc36f052c021430
rossi.kwak <rossi.kwak@gmail.com>
2017-08-17 06:45:36 +0000

    init2 함수 와 테스트 코드 추가

--- v0.1/hsmProxy/hsm.cpp
+++ v0.1/hsmProxy/hsm.cpp
@@ -1,6 +1,7 @@
 // hsm.cpp 
 #include <node.h>
 #include "CHsmProxy.h"
+#include <string.h>
 
 namespace demo {
 
@@ -12,6 +13,11 @@ using v8::Local;
 using v8::Object;
 
 CHsmProxy hsm;
+
+const char* ToCString(const String::Utf8Value& value)
+{
+  return *value ? *value : "<string conversion failed>";
+}
 
 void Init(const FunctionCallbackInfo<Value>& args)
 {
@@ -25,8 +31,31 @@ void Init(const FunctionCallbackInfo<Value>& args)
   args.GetReturnValue().Set(nRtn);
 }
 
+void Init2(const FunctionCallbackInfo<Value>& args)
+{
+  printf("Init2\n");
+  String::Utf8Value strLabel(args[0]);
+  const char* label = ToCString(strLabel);
+
+  String::Utf8Value strSoPin(args[1]);
+  const char* soPin = ToCString(strSoPin);
+
+  String::Utf8Value strUserPin(args[2]);
+  const char* userPin = ToCString(strUserPin);
+
+  String::Utf8Value strEmptySlot(args[3]);
+  const char* emptySlot = ToCString(strEmptySlot);
+
+  bool bEmptySlot = true;
+  if(strcmp(emptySlot,"false")==0) bEmptySlot=false;
+
+  int nRtn = hsm.init(label,soPin,userPin,bEmptySlot);
+  args.GetReturnValue().Set(nRtn);
+}
+
 void init(Local<Object> exports) {
   NODE_SET_METHOD(exports, "init", Init);
+  NODE_SET_METHOD(exports, "init2", Init2);
 }
 
 NODE_MODULE(addon, init)
--- v0.1/hsmProxy/testHsm.js
+++ v0.1/hsmProxy/testHsm.js
@@ -4,3 +4,13 @@ console.log(hsm);
 
 var userPin="1234";
 console.log("hsm.init("+userPin+")="+hsm.init(userPin));
+
+var soPin = "123456";
+var label = "MyLabel";
+var emptySlot = true;
+console.log("hsm.init2("+label+","+soPin+","+userPin+","+emptySlot+")="
+	+hsm.init2(label,soPin,userPin,emptySlot));
+
+emptySlot = false;
+console.log("hsm.init2("+label+","+soPin+","+userPin+","+emptySlot+")="
+	+hsm.init2(label,soPin,userPin,emptySlot));
