fe55a58453e55e709d8b0350badecd11f19a966c
rossi.kwak <rossi.kwak@gmail.com>
2017-08-17 13:46:30 +0000

    에러 메세지 표시

--- v0.1/hsmProxy/simHsm.js
+++ v0.1/hsmProxy/simHsm.js
@@ -6,9 +6,10 @@ var value = "./softhsm2-linux.conf";
 var overwrite = 1;
 console.log("hsm.setEnv("+name+","+value+","+overwrite+")="+hsm.setEnv(name,value,overwrite));
 
-var userPin="1234";
+var userPin="12345";
 if(hsm.init(userPin)!=0) {
   console.log("HSM init failurei(userPin:"+userPin+")");
+  console.log("error message:"+hsm.message());
   process.exit(-1);
 }
 console.log("hsm.init("+userPin+") ok");
@@ -19,6 +20,7 @@ console.log("TagKeyLabel="+TAG_KEY_LABEL+",TagKeyLabel length="+TAG_KEY_LABEL.le
 
 if(hsm.findKey(TAG_KEY_LABEL,TAG_KEY_LABEL.length)!=0){
   console.log("findKey("+TAG_KEY_LABEL+") failed");
+  console.log("error message:"+hsm.message());
   process.exit(-1);
 }
 var hTagKey = hsm.getFoundKey(); 
@@ -29,6 +31,7 @@ console.log("SeKeyLabel="+SE_KEY_LABEL+",SeKeyLabel length="+SE_KEY_LABEL.length
 
 if(hsm.findKey(SE_KEY_LABEL,SE_KEY_LABEL.length)!=0){
   console.log("findKey("+SE_KEY_LABEL+") failed");
+  console.log("error message:"+hsm.message());
   process.exit(-1);
 }
 var hSeKey = hsm.getFoundKey(); 
@@ -37,6 +40,7 @@ console.log("SE Key found, Found key="+hSeKey);
 var mType = "AES_ECB";
 if(hsm.encryptInit(mType,hTagKey)!=0) {
   console.log("hsm.encryptInit("+mType+","+hTagKey+") failed");
+  console.log("error message:"+hsm.message());
   process.exit(-1);
 }
 console.log("hsm.encryptInit("+mType+","+hTagKey+") ok");
@@ -55,6 +59,7 @@ console.log("after encoding="+encBuf.toString()+",length="+encBuf.length);
 
 if(hsm.decryptInit(mType,hTagKey)!=0) {
   console.log("hsm.decryptInit("+mType+","+hTagKey+") failed");
+  console.log("error message:"+hsm.message());
   process.exit(-1);
 }
 console.log("hsm.decryptInit("+mType+","+hTagKey+") ok");
