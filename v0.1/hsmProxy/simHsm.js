5203a9a7fa9a52bc3152d1c92de97a0b4b46350b
rossi.kwak <rossi.kwak@gmail.com>
2017-08-17 08:31:06 +0000

    hTadKey와 hSeKey가 값이 할당 안된 버그 수정, encryptInit호출 구현

--- v0.1/hsmProxy/simHsm.js
+++ v0.1/hsmProxy/simHsm.js
@@ -17,19 +17,26 @@ console.log("SlotID="+hsm.slotId());
 var TAG_KEY_LABEL = "GroupTagKey";
 console.log("TagKeyLabel="+TAG_KEY_LABEL+",TagKeyLabel length="+TAG_KEY_LABEL.length);
 
-var hTagKey
 if(hsm.findKey(TAG_KEY_LABEL,TAG_KEY_LABEL.length)!=0){
   console.log("findKey("+TAG_KEY_LABEL+") failed");
   process.exit(-1);
 }
-console.log("Tag Key found, Found key="+hsm.getFoundKey());
+var hTagKey = hsm.getFoundKey(); 
+console.log("Tag Key found, Found key="+hTagKey);
 
 var SE_KEY_LABEL = "SeKey";
 console.log("SeKeyLabel="+SE_KEY_LABEL+",SeKeyLabel length="+SE_KEY_LABEL.length);
 
-var hSeKey
 if(hsm.findKey(SE_KEY_LABEL,SE_KEY_LABEL.length)!=0){
   console.log("findKey("+SE_KEY_LABEL+") failed");
   process.exit(-1);
 }
-console.log("SE Key found, Found key="+hsm.getFoundKey());
+var hSeKey = hsm.getFoundKey(); 
+console.log("SE Key found, Found key="+hSeKey);
+
+var mType = "AES_ECB";
+if(hsm.encryptInit(mType,hTagKey)!=0) {
+  console.log("hsm.encryptInit("+mType+","+hTagKey+") failed");
+  process.exit(-1);
+}
+console.log("hsm.encryptInit("+mType+","+hTagKey+") ok");
