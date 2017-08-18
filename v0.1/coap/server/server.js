fbca339483383f045fe98d1fc6c42a72310c19c3
rossi.kwak <rossi.kwak@gmail.com>
2017-08-18 05:36:22 +0000

    * node-coap(https://github.com/mcollina/node-coap)이라는 클라이언트/서버 라이브러리를 사용한다. 설치후 coap실행에 필요한 최소한의 파일(index.js)과 폴더(lib,node_modules_만 추가하였다. 이 라이브러리를 응용하여 작성한 서버를 server폴더에 저장하였다.

---
+++ v0.1/coap/server/server.js
@@ -0,0 +1,12 @@
+//server.js
+const coap = require('../')
+
+server = coap.createServer()
+
+server.on('request',function(req,res){
+  res.end('Hello ' + req.url.split('/')[1] + '\n')
+})
+
+server.listen(function(){
+  console.log('server started')
+})
