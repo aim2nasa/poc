fb72ecce5908f02f2c97858a232290f1dfdc2857
rossi.kwak <rossi.kwak@gmail.com>
2017-08-18 07:30:54 +0000

    * payload에 데이터를 넣어 클라이언트에서 보내면 서버에서 받아 표시하는 기능 테스트

--- v0.1/coap/client/client.js
+++ v0.1/coap/client/client.js
@@ -1,10 +1,19 @@
 //client.js
 const coap = require('../')
 
-req = coap.request('coap://localhost/Rossi')
+var coapConnection = {
+  host:'localhost',
+  pathname: '/payloadTest',
+  method: 'GET',
+  confirmable:true
+}
+
+var req = coap.request(coapConnection)
+req.write('ABCDEFG')
 
 req.on('response',function(res){
   res.pipe(process.stdout);
+  //console.log(res)
 })
 
 req.end()
--- v0.1/coap/server/server.js
+++ v0.1/coap/server/server.js
@@ -4,11 +4,12 @@ const coap = require('../')
 server = coap.createServer()
 
 server.on('request',function(req,res){
-  console.log('===============Request=====================\n');
-  console.log(req);
+  //console.log('===============Request=====================\n');
+  //console.log(req);
+  console.log('req.payload='+req.payload);
   res.end('Hello ' + req.url.split('/')[1] + '\n')
-  console.log('===============Response====================\n');
-  console.log(res);
+  //console.log('===============Response====================\n');
+  //console.log(res);
 })
 
 server.listen(function(){
