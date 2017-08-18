8b1805a42b2da4dc6181e3651c65dc3c4ae2cc05
rossi.kwak <rossi.kwak@gmail.com>
2017-08-18 07:23:15 +0000

    화면에 req,res출력하도록 수정

--- v0.1/coap/server/server.js
+++ v0.1/coap/server/server.js
@@ -4,7 +4,11 @@ const coap = require('../')
 server = coap.createServer()
 
 server.on('request',function(req,res){
+  console.log('===============Request=====================\n');
+  console.log(req);
   res.end('Hello ' + req.url.split('/')[1] + '\n')
+  console.log('===============Response====================\n');
+  console.log(res);
 })
 
 server.listen(function(){
