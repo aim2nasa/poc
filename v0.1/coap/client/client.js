f87d8c58b552bfd2ae7653e1dc20503612c9aff3
rossi.kwak <rossi.kwak@gmail.com>
2017-08-18 08:51:39 +0000

    TAG키를 읽어오는 구현 적용

--- v0.1/coap/client/client.js
+++ v0.1/coap/client/client.js
@@ -14,6 +14,18 @@ if(hsm.init(userPin)!=0) {
 console.log("hsm.init("+userPin+") ok");
 console.log("SlotID="+hsm.slotId());
 
+//Tag키 읽어 오기
+var TAG_KEY_LABEL = "GroupTagKey";
+console.log("TagKeyLabel="+TAG_KEY_LABEL+",TagKeyLabel length="+TAG_KEY_LABEL.length);
+
+if(hsm.findKey(TAG_KEY_LABEL,TAG_KEY_LABEL.length)!=0){
+  console.log("findKey("+TAG_KEY_LABEL+") failed");
+  console.log("error message:"+hsm.message());
+  process.exit(-1);
+}
+var hTagKey = hsm.getFoundKey();
+console.log("Tag Key found, Found key="+hTagKey);
+
 var coapConnection = {
   host:'localhost',
   pathname: '/payloadTest',
