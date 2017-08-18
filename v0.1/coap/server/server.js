ca41c1eac36110b8967d802283e4fccc34a9e53f
rossi.kwak <rossi.kwak@gmail.com>
2017-08-18 09:13:47 +0000

    hsm을 초기화하고 슬롯을 읽어오는 구현 적용

--- v0.1/coap/server/server.js
+++ v0.1/coap/server/server.js
@@ -5,6 +5,15 @@ const hsm = require('../../hsmProxy/build/Release/hsmproxy');
 var rtn = hsm.setEnv("SOFTHSM2_CONF","./softhsm2-linux.conf",1)
 console.log('hsm.setEnv='+rtn)
 
+var userPin="1234";
+if(hsm.init(userPin)!=0) {
+  console.log("HSM init failure(userPin:"+userPin+")");
+  console.log("error message:"+hsm.message());
+  process.exit(-1);
+}
+console.log("hsm.init("+userPin+") ok");
+console.log("SlotID="+hsm.slotId());
+
 server = coap.createServer()
 
 server.on('request',function(req,res){
