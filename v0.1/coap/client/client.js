5405b3902f4e8ec32a65c2fa84be3ccb86dda1e7
Rossi Kwak <rossi.kwak@gmail.com>
2017-09-11 19:17:53 +0900

    observeMode를 true/false로 읽어 동작하도록 수정함, observeMode값을 처리하는데 있어서 javascript 주의할점이 있음, coapConnection.observe=false 이렇게 되면 원하는 동작이 안되고 반드시 coapConnection.observe=false 해야 함. 이유는 false는 문자열 타입이고 false는 boolean타입이기 때문이다. false==false 논리연산하면 거짓이 된다. 타입이 다르기 때문에.. 주의해야 함vi client.js

--- v0.1/coap/client/client.js
+++ v0.1/coap/client/client.js
@@ -5,16 +5,21 @@ const hsm = require('../../hsmProxy/build/Release/hsmproxy');
 var argc = process.argv.length; 
 console.log('arguments='+argc);
 if(argc<4) {
-  console.log('usage: client <server> <resource>'); 
+  console.log('usage: client <server> <resource> <observeMode>'); 
   console.log('       <server> : localhost for local server(127.0.0.1)'); 
   console.log('       <resource> : supported resource must be one of the followings'); 
   console.log('                    date,version'); 
+  console.log('       <observeMode> : true or false (default is false)'); 
   process.exit(-1);
 }
 var server = process.argv[2];
 var resource = process.argv[3];
+var observeMode = false; //default
 console.log('server='+server);
 console.log('resource='+resource);
+
+if(argc>=5) observeMode = process.argv[4];
+console.log('observe='+observeMode);
 
 var rtn = hsm.setEnv("SOFTHSM2_CONF","./softhsm2-linux.conf",1)
 console.log('hsm.setEnv='+rtn)
@@ -59,20 +64,20 @@ console.log("before encoding="+buf.toString()+",length="+buf.length);
 var encBuf = hsm.encrypt(buf,buf.length);
 console.log("after encoding="+encBuf.toString()+",length="+encBuf.length);
 
-//resource에 따른 observe모드 설정
-var observeMode = false;
-if(resource=='date'){
-  observeMode = true;
-}
-
-console.log('observe='+observeMode);
 var coapConnection = {
   host:server,
   pathname: '/'+resource,
   method: 'GET',
-  confirmable:true,
-  observe:observeMode
+  confirmable:true
 }
+
+if(observeMode=='true'){
+  coapConnection.observe = true;
+}else{
+  coapConnection.observe = false;
+}
+
+console.log('coapConnection=%j', coapConnection);
 
 var req = coap.request(coapConnection)
 req.write(encBuf)
