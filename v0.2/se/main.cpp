93d17e57770b5b7dd334eb63a93ce2b51ae373d0
Rossi <rossi.kwak@gmail.com>
2018-04-09 07:34:23 +0000

    remove function declaration and move main function to be defined at the end

--- v0.2/se/main.cpp
+++ v0.2/se/main.cpp
@@ -25,21 +25,175 @@ static u_short SERVER_PORT = 9876;
 #ifdef USE_SSL
 #define SE_SOCK_STREAM ACE_SSL_SOCK_Stream
 #define SE_SOCK_CONNECTOR ACE_SSL_SOCK_Connector
-void certInfo(SSL *ssl);
+static void certInfo(SSL *ssl)
+{
+	X509* cert = ::SSL_get_peer_certificate(ssl);
+	ACE_ASSERT(cert);
+
+	char * retString = NULL;
+	// 주체의 DN을 문자열로 얻음
+	retString = X509_NAME_oneline(X509_get_subject_name(cert), 0, 0);
+	if (retString == NULL) {
+		std::cout << "서버 인증서에서 주체의 DN을 읽을 수 없음";
+		return;
+	}
+	std::cout << " -subject:" << retString << std::endl;
+	OPENSSL_free(retString);
+
+	// 발급자의 DN을 문자열로 얻음
+	retString = X509_NAME_oneline(X509_get_issuer_name(cert), 0, 0);
+	if (retString == NULL) {
+		std::cout << "서버 인증서에서 발급자의 DN을 읽을 수 없음";
+		return;
+	}
+	std::cout << " -issuer:" << retString << std::endl;
+	OPENSSL_free(retString);
+	X509_free(cert);
+}
 #else
 #define SE_SOCK_STREAM ACE_SOCK_Stream
 #define SE_SOCK_CONNECTOR ACE_SOCK_Connector
 #endif
 
-static int createSerialNo(CToken &token, char *sn, unsigned int snSize);
-static size_t send(SE_SOCK_STREAM &sock, const char *buffer, size_t len);
-static int sendSerialNo(SE_SOCK_STREAM &sock, const char *serialNo);
-static int registerKeys(SE_SOCK_STREAM &sock, CK_SESSION_HANDLE hSession);
-static int onNtfTags(const char *buffer, unsigned int len, CK_SESSION_HANDLE hSession);
-static int onTagKey(const char *buffer, unsigned int len, CK_SESSION_HANDLE hSession);
-static int onSeKey(const char *buffer, unsigned int len, CK_SESSION_HANDLE hSession);
-static int aesKeyInjection(CK_BYTE_PTR key, CK_ULONG keySize, CK_SESSION_HANDLE hSession, CK_OBJECT_HANDLE &hKey, const char *label);
-static void displayKey(const char *name,const char *buffer, unsigned int len);
+static void displayKey(const char *name,const char *buffer, unsigned int len)
+{
+	ACE_DEBUG((LM_DEBUG, "%s:%d bytes\n",name,len));
+	for(unsigned int i=0;i<len;i++) 
+		ACE_DEBUG((LM_DEBUG, "%x ",buffer[i]));
+	ACE_DEBUG((LM_DEBUG, "\n"));
+}
+
+static int aesKeyInjection(CK_BYTE_PTR key, CK_ULONG keySize, CK_SESSION_HANDLE hSession, CK_OBJECT_HANDLE &hKey, const char *label)
+{
+	//원하는 키값으로 객체를 생성한다.
+	CK_BBOOL bTrue = CK_TRUE;
+	CK_BBOOL bFalse = CK_FALSE;
+	CK_OBJECT_CLASS secretClass = CKO_SECRET_KEY;
+	CK_KEY_TYPE keyType = CKK_GENERIC_SECRET;
+	CK_ATTRIBUTE attribs[] = {
+		{ CKA_VALUE, key, keySize },
+		{ CKA_EXTRACTABLE, &bFalse, sizeof(bFalse) },
+		{ CKA_CLASS, &secretClass, sizeof(secretClass) },
+		{ CKA_KEY_TYPE, &keyType, sizeof(keyType) },
+		{ CKA_TOKEN, &bTrue, sizeof(bTrue) },
+		{ CKA_PRIVATE, &bFalse, sizeof(bFalse) },
+		{ CKA_SENSITIVE, &bFalse, sizeof(bTrue) },
+		{ CKA_LABEL, (CK_UTF8CHAR_PTR)label, strlen(label) }
+	};
+
+	CK_RV rv;
+	hKey = CK_INVALID_HANDLE;
+	if((rv=C_CreateObject(hSession, attribs, sizeof(attribs) / sizeof(CK_ATTRIBUTE), &hKey))!=CKR_OK)
+		ACE_ERROR_RETURN((LM_ERROR, ACE_TEXT("(%t) Error(0x%x) in C_CreateObject(session:%d)\n"), rv,hSession), -1);
+
+	ACE_RETURN(0);
+}
+
+static int onTagKey(const char *buffer, unsigned int len, CK_SESSION_HANDLE hSession)
+{
+	ACE_ASSERT(len == AES_KEY_SIZE);
+	displayKey("TAG KEY",buffer,len);
+
+	CK_OBJECT_HANDLE hTagKey = CK_INVALID_HANDLE;
+	if (aesKeyInjection((CK_BYTE_PTR)(buffer) ,len, hSession, hTagKey, TAG_KEY_LABEL)!=0) 
+		ACE_ERROR_RETURN((LM_ERROR, ACE_TEXT("(%t) Error in aesKeyInjection(session:%d)\n"), hSession), -1);
+
+	ACE_ASSERT(hTagKey != CK_INVALID_HANDLE);
+	ACE_RETURN(0);
+}
+
+static int onSeKey(const char *buffer, unsigned int len, CK_SESSION_HANDLE hSession)
+{
+	ACE_ASSERT(len == AES_KEY_SIZE);
+	displayKey("SE KEY",buffer,len);
+
+	CK_OBJECT_HANDLE hSeKey = CK_INVALID_HANDLE;
+	if (aesKeyInjection((CK_BYTE_PTR)(buffer), len, hSession, hSeKey, SE_KEY_LABEL) != 0)
+		ACE_ERROR_RETURN((LM_ERROR, ACE_TEXT("(%t) Error in aesKeyInjection(session:%d)\n"), hSession), -1);
+
+	ACE_ASSERT(hSeKey != CK_INVALID_HANDLE);
+	ACE_RETURN(0);
+}
+
+static int onNtfTags(const char *buffer, unsigned int len, CK_SESSION_HANDLE hSession)
+{
+	ACE_ASSERT(len%AES_KEY_SIZE == 0);
+
+	unsigned int count = len / AES_KEY_SIZE;
+	ACE_DEBUG((LM_DEBUG, "NTF TAGS received keys:%d\n", count));
+	for (unsigned int i = 0; i < count; i++) {
+		(i == 0) ? onTagKey(buffer,AES_KEY_SIZE,hSession) : onSeKey(buffer,AES_KEY_SIZE,hSession);
+		buffer += AES_KEY_SIZE;
+	}
+	ACE_RETURN(0);
+}
+
+static int createSerialNo(CToken &token, char *sn, unsigned int snSize)
+{
+	if (token.genRandom(sn, snSize) != 0)
+		ACE_ERROR_RETURN((LM_ERROR, "(%P|%t) %p \n", "SE serialNo creation failed"), -1);
+
+	ACE_DEBUG((LM_INFO, "(%t) serialNo:"));
+	for (unsigned long i = 0; i < snSize; i++) ACE_DEBUG((LM_INFO, ACE_TEXT("%0x "), static_cast<unsigned char>(sn[i])));
+	ACE_DEBUG((LM_INFO, "\n"));
+
+	ACE_RETURN(0);
+}
+
+static size_t send(SE_SOCK_STREAM &sock, const char *buffer, size_t len)
+{
+	return sock.send_n(buffer,len);
+}
+
+static int sendSerialNo(SE_SOCK_STREAM &sock, const char *serialNo)
+{
+	ssize_t send_cnt = 0;
+
+	//프로토콜 포맷 정의
+	//{prefix, 8바이트} {dataSize,바이트} {data}
+
+	//prefix
+	if ((send_cnt = send(sock, PRF_SERIALNO, PREFIX_SIZE)) == -1)
+		ACE_ERROR_RETURN((LM_ERROR, "(%P|%t) %p \n", "Error send(%d), prefix", send_cnt), -1);
+
+	//dataSize
+	ACE_INT32 dataSize = ACE_HTONL(ACE_Utils::truncate_cast<ACE_INT32> (SERIAL_NO_SIZE));
+	if ((send_cnt = send(sock, reinterpret_cast<const char*>(&dataSize), sizeof(ACE_INT32))) == -1)
+		ACE_ERROR_RETURN((LM_ERROR, "(%P|%t) %p \n", "Error send(%d), dataSize", send_cnt), -1);
+
+	//serialNo
+	if ((send_cnt = send(sock, reinterpret_cast<const char*>(serialNo), SERIAL_NO_SIZE)) == -1)
+		ACE_ERROR_RETURN((LM_ERROR, "(%P|%t) %p \n", "Error send(%d), serialNo", send_cnt), -1);
+
+	ACE_RETURN(0);
+}
+
+static int registerKeys(SE_SOCK_STREAM &sock, CK_SESSION_HANDLE hSession)
+{
+	ssize_t recv_cnt = 0;
+	char buffer[SIZE_BUF];
+
+	//prefix
+	if ((recv_cnt = sock.recv_n(buffer, PREFIX_SIZE)) == -1)
+		ACE_ERROR_RETURN((LM_ERROR, "(%P|%t) %p \n", "Error recv_n(%d), prefix", recv_cnt), -1);
+
+	buffer[PREFIX_SIZE] = 0;
+	std::string prefix = buffer;
+
+	//dataSize
+	ACE_INT32 dataSize;
+	if ((recv_cnt = sock.recv_n(&dataSize, sizeof(ACE_INT32))) <= 0)
+		ACE_ERROR_RETURN((LM_ERROR, "(%P|%t) %p \n", "Error recv_n(%d), dataSize", recv_cnt), -1);
+
+	//data
+	if (dataSize > 0) {
+		if ((recv_cnt = sock.recv_n(buffer, dataSize)) <= 0)
+			ACE_ERROR_RETURN((LM_ERROR, "(%P|%t) %p \n", "Error recv_n(%d), data", recv_cnt), -1);
+	}
+
+	if (prefix == PRF_NTF_TAGS) onNtfTags(buffer, dataSize, hSession);
+	ACE_RETURN(0);
+}
 
 int main(int argc, char *argv[])
 {
@@ -113,171 +267,3 @@ int main(int argc, char *argv[])
 	ACE_DEBUG((LM_INFO, "(%t) SE successfully initialized\n"));
 	return 0;
 }
-
-static int createSerialNo(CToken &token, char *sn, unsigned int snSize)
-{
-	if (token.genRandom(sn, snSize) != 0)
-		ACE_ERROR_RETURN((LM_ERROR, "(%P|%t) %p \n", "SE serialNo creation failed"), -1);
-
-	ACE_DEBUG((LM_INFO, "(%t) serialNo:"));
-	for (unsigned long i = 0; i < snSize; i++) ACE_DEBUG((LM_INFO, ACE_TEXT("%0x "), static_cast<unsigned char>(sn[i])));
-	ACE_DEBUG((LM_INFO, "\n"));
-
-	ACE_RETURN(0);
-}
-
-static size_t send(SE_SOCK_STREAM &sock, const char *buffer, size_t len)
-{
-	return sock.send_n(buffer,len);
-}
-
-static int sendSerialNo(SE_SOCK_STREAM &sock, const char *serialNo)
-{
-	ssize_t send_cnt = 0;
-
-	//프로토콜 포맷 정의
-	//{prefix, 8바이트} {dataSize,바이트} {data}
-
-	//prefix
-	if ((send_cnt = send(sock, PRF_SERIALNO, PREFIX_SIZE)) == -1)
-		ACE_ERROR_RETURN((LM_ERROR, "(%P|%t) %p \n", "Error send(%d), prefix", send_cnt), -1);
-
-	//dataSize
-	ACE_INT32 dataSize = ACE_HTONL(ACE_Utils::truncate_cast<ACE_INT32> (SERIAL_NO_SIZE));
-	if ((send_cnt = send(sock, reinterpret_cast<const char*>(&dataSize), sizeof(ACE_INT32))) == -1)
-		ACE_ERROR_RETURN((LM_ERROR, "(%P|%t) %p \n", "Error send(%d), dataSize", send_cnt), -1);
-
-	//serialNo
-	if ((send_cnt = send(sock, reinterpret_cast<const char*>(serialNo), SERIAL_NO_SIZE)) == -1)
-		ACE_ERROR_RETURN((LM_ERROR, "(%P|%t) %p \n", "Error send(%d), serialNo", send_cnt), -1);
-
-	ACE_RETURN(0);
-}
-
-static int registerKeys(SE_SOCK_STREAM &sock, CK_SESSION_HANDLE hSession)
-{
-	ssize_t recv_cnt = 0;
-	char buffer[SIZE_BUF];
-
-	//prefix
-	if ((recv_cnt = sock.recv_n(buffer, PREFIX_SIZE)) == -1)
-		ACE_ERROR_RETURN((LM_ERROR, "(%P|%t) %p \n", "Error recv_n(%d), prefix", recv_cnt), -1);
-
-	buffer[PREFIX_SIZE] = 0;
-	std::string prefix = buffer;
-
-	//dataSize
-	ACE_INT32 dataSize;
-	if ((recv_cnt = sock.recv_n(&dataSize, sizeof(ACE_INT32))) <= 0)
-		ACE_ERROR_RETURN((LM_ERROR, "(%P|%t) %p \n", "Error recv_n(%d), dataSize", recv_cnt), -1);
-
-	//data
-	if (dataSize > 0) {
-		if ((recv_cnt = sock.recv_n(buffer, dataSize)) <= 0)
-			ACE_ERROR_RETURN((LM_ERROR, "(%P|%t) %p \n", "Error recv_n(%d), data", recv_cnt), -1);
-	}
-
-	if (prefix == PRF_NTF_TAGS) onNtfTags(buffer, dataSize, hSession);
-	ACE_RETURN(0);
-}
-
-static int onNtfTags(const char *buffer, unsigned int len, CK_SESSION_HANDLE hSession)
-{
-	ACE_ASSERT(len%AES_KEY_SIZE == 0);
-
-	unsigned int count = len / AES_KEY_SIZE;
-	ACE_DEBUG((LM_DEBUG, "NTF TAGS received keys:%d\n", count));
-	for (unsigned int i = 0; i < count; i++) {
-		(i == 0) ? onTagKey(buffer,AES_KEY_SIZE,hSession) : onSeKey(buffer,AES_KEY_SIZE,hSession);
-		buffer += AES_KEY_SIZE;
-	}
-	ACE_RETURN(0);
-}
-
-static void displayKey(const char *name,const char *buffer, unsigned int len)
-{
-	ACE_DEBUG((LM_DEBUG, "%s:%d bytes\n",name,len));
-	for(unsigned int i=0;i<len;i++) 
-		ACE_DEBUG((LM_DEBUG, "%x ",buffer[i]));
-	ACE_DEBUG((LM_DEBUG, "\n"));
-}
-
-static int onTagKey(const char *buffer, unsigned int len, CK_SESSION_HANDLE hSession)
-{
-	ACE_ASSERT(len == AES_KEY_SIZE);
-	displayKey("TAG KEY",buffer,len);
-
-	CK_OBJECT_HANDLE hTagKey = CK_INVALID_HANDLE;
-	if (aesKeyInjection((CK_BYTE_PTR)(buffer) ,len, hSession, hTagKey, TAG_KEY_LABEL)!=0) 
-		ACE_ERROR_RETURN((LM_ERROR, ACE_TEXT("(%t) Error in aesKeyInjection(session:%d)\n"), hSession), -1);
-
-	ACE_ASSERT(hTagKey != CK_INVALID_HANDLE);
-	ACE_RETURN(0);
-}
-
-static int onSeKey(const char *buffer, unsigned int len, CK_SESSION_HANDLE hSession)
-{
-	ACE_ASSERT(len == AES_KEY_SIZE);
-	displayKey("SE KEY",buffer,len);
-
-	CK_OBJECT_HANDLE hSeKey = CK_INVALID_HANDLE;
-	if (aesKeyInjection((CK_BYTE_PTR)(buffer), len, hSession, hSeKey, SE_KEY_LABEL) != 0)
-		ACE_ERROR_RETURN((LM_ERROR, ACE_TEXT("(%t) Error in aesKeyInjection(session:%d)\n"), hSession), -1);
-
-	ACE_ASSERT(hSeKey != CK_INVALID_HANDLE);
-	ACE_RETURN(0);
-}
-
-static int aesKeyInjection(CK_BYTE_PTR key, CK_ULONG keySize, CK_SESSION_HANDLE hSession, CK_OBJECT_HANDLE &hKey, const char *label)
-{
-	//원하는 키값으로 객체를 생성한다.
-	CK_BBOOL bTrue = CK_TRUE;
-	CK_BBOOL bFalse = CK_FALSE;
-	CK_OBJECT_CLASS secretClass = CKO_SECRET_KEY;
-	CK_KEY_TYPE keyType = CKK_GENERIC_SECRET;
-	CK_ATTRIBUTE attribs[] = {
-		{ CKA_VALUE, key, keySize },
-		{ CKA_EXTRACTABLE, &bFalse, sizeof(bFalse) },
-		{ CKA_CLASS, &secretClass, sizeof(secretClass) },
-		{ CKA_KEY_TYPE, &keyType, sizeof(keyType) },
-		{ CKA_TOKEN, &bTrue, sizeof(bTrue) },
-		{ CKA_PRIVATE, &bFalse, sizeof(bFalse) },
-		{ CKA_SENSITIVE, &bFalse, sizeof(bTrue) },
-		{ CKA_LABEL, (CK_UTF8CHAR_PTR)label, strlen(label) }
-	};
-
-	CK_RV rv;
-	hKey = CK_INVALID_HANDLE;
-	if((rv=C_CreateObject(hSession, attribs, sizeof(attribs) / sizeof(CK_ATTRIBUTE), &hKey))!=CKR_OK)
-		ACE_ERROR_RETURN((LM_ERROR, ACE_TEXT("(%t) Error(0x%x) in C_CreateObject(session:%d)\n"), rv,hSession), -1);
-
-	ACE_RETURN(0);
-}
-
-#ifdef USE_SSL
-static void certInfo(SSL *ssl)
-{
-	X509* cert = ::SSL_get_peer_certificate(ssl);
-	ACE_ASSERT(cert);
-
-	char * retString = NULL;
-	// 주체의 DN을 문자열로 얻음
-	retString = X509_NAME_oneline(X509_get_subject_name(cert), 0, 0);
-	if (retString == NULL) {
-		std::cout << "서버 인증서에서 주체의 DN을 읽을 수 없음";
-		return;
-	}
-	std::cout << " -subject:" << retString << std::endl;
-	OPENSSL_free(retString);
-
-	// 발급자의 DN을 문자열로 얻음
-	retString = X509_NAME_oneline(X509_get_issuer_name(cert), 0, 0);
-	if (retString == NULL) {
-		std::cout << "서버 인증서에서 발급자의 DN을 읽을 수 없음";
-		return;
-	}
-	std::cout << " -issuer:" << retString << std::endl;
-	OPENSSL_free(retString);
-	X509_free(cert);
-}
-#endif
