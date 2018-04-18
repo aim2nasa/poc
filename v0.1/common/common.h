e7255edec33e98448c5e954e916292da8c68b009
Rossi <rossi.kwak@gmail.com>
2018-04-18 09:03:30 +0000

    as default SSL is not defined. if you want to compile the project as SSL active, then you need to add flag like -D USE_SSL

--- v0.1/common/common.h
+++ v0.1/common/common.h
@@ -7,8 +7,6 @@
 #define TAG_KEY_LABEL	"GroupTagKey"	//Label given for each Group Tag
 #define SE_KEY_LABEL	"SeKey"		//Label giben for each SE
 
-#define USE_SSL				//If we use SSL(ACE SSL) then USE_SSL must be defined Otherwise SSL disabled 
-
 #ifdef _WIN32
 #define MODEXPORT __declspec(dllexport)
 #else
