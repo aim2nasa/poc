4dae2a2ae960729eca173c538380f0a9fc43bdf0
Rossi <rossi.kwak@gmail.com>
2018-04-20 04:21:54 +0000

    define when USE_OPTEE is declared

--- v0.2/tokenServer/CClientAcceptor.h
+++ v0.2/tokenServer/CClientAcceptor.h
@@ -3,7 +3,11 @@
 
 #include "Stream_Handler.h"
 
+#ifdef USE_SOFTHSM
 class CHsmProxy;
+#elif defined(USE_OPTEE)
+#include <okey.h>
+#endif
 
 class CClientAcceptor : public ACE_Acceptor<Stream_Handler, ACE_SOCK_ACCEPTOR>
 {
@@ -12,6 +16,10 @@ public:
 	CClientAcceptor() :pHsm_(NULL), hTagKey_(0), hSeKey_(0){}
 	CHsmProxy *pHsm_;
 	unsigned long hTagKey_, hSeKey_;
+#elif defined(USE_OPTEE)
+	CClientAcceptor() :pO_(NULL), tagKey_(0), seKey_(0){}
+	okey *pO_;
+	uint32_t tagKey_, seKey_;
 #endif
 };
 
