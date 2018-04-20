a9b5d2bc8270c63c6d7ec66cd26791d4f79c2629
Rossi <rossi.kwak@gmail.com>
2018-04-20 04:03:01 +0000

    display options need when USE_OPTEE is enabled, initialize libokey and retrieve tag,se keys from the secure storage

--- v0.2/tokenServer/main.cpp
+++ v0.2/tokenServer/main.cpp
@@ -11,6 +11,7 @@
 #include <ace/Reactor_Notification_Strategy.h>
 #include "CClientAcceptor.h"
 
+#include <common.h>
 #ifdef USE_SOFTHSM
 #include "CHsmProxy.h"
 #elif USE_OPTEE
@@ -25,6 +26,13 @@ int ACE_TMAIN(int argc, ACE_TCHAR *argv[])
 	ACE_DEBUG((LM_INFO, "configured to use softhsm\n"));
 	if (argc<3) {
 		ACE_ERROR((LM_ERROR, ACE_TEXT("usage:tokenServer <port> <userPin>\n")));
+		ACE_ERROR((LM_ERROR, ACE_TEXT("      port:set 0 for defalut port(9870)\n")));
+		ACE_RETURN(-1);
+	}
+#elif defined(USE_OPTEE)
+	ACE_DEBUG((LM_INFO, "configured to use optee\n"));
+	if (argc<2) {
+		ACE_ERROR((LM_ERROR, ACE_TEXT("usage:tokenServer <port>\n")));
 		ACE_ERROR((LM_ERROR, ACE_TEXT("      port:set 0 for defalut port(9870)\n")));
 		ACE_RETURN(-1);
 	}
@@ -63,6 +71,26 @@ int ACE_TMAIN(int argc, ACE_TCHAR *argv[])
 
 	ACE_ASSERT(hSeKey != 0);
 	ACE_DEBUG((LM_INFO, "(%t) SE key(%d) retrieved\n", hSeKey));
+#elif defined(USE_OPTEE)
+	okey o;
+	TEEC_Result res = initializeContext(NULL,&o);
+	if(res!=TEEC_SUCCESS)
+		ACE_ERROR_RETURN((LM_ERROR, ACE_TEXT("(%P|%t) ") ACE_TEXT("initializeContext failed 0x%x"),res), -1);
+
+	res = openSession(&o,TEEC_LOGIN_PUBLIC,NULL,NULL);
+	if(res!=TEEC_SUCCESS)
+		ACE_ERROR_RETURN((LM_ERROR, ACE_TEXT("(%P|%t) ") ACE_TEXT("openSession failed 0x%x"),res), -1);
+
+	uint32_t tagKey,seKey;
+	res = keyOpen(&o,TEE_STORAGE_PRIVATE,TAG_KEY_LABEL,&tagKey);
+	if(res!=TEEC_SUCCESS)
+		ACE_ERROR_RETURN((LM_ERROR, ACE_TEXT("(%P|%t) ") ACE_TEXT("keyOpen(%s) failed 0x%x"),TAG_KEY_LABEL,res), -1);
+	ACE_DEBUG((LM_INFO, "(%t) Tag key(0x%x) retrieved\n", tagKey));
+
+	res = keyOpen(&o,TEE_STORAGE_PRIVATE,SE_KEY_LABEL,&seKey);
+	if(res!=TEEC_SUCCESS)
+		ACE_ERROR_RETURN((LM_ERROR, ACE_TEXT("(%P|%t) ") ACE_TEXT("keyOpen(%s) failed 0x%x"),SE_KEY_LABEL,res), -1);
+	ACE_DEBUG((LM_INFO, "(%t) SE key(0x%x) retrieved\n", seKey));
 #endif //USE_SOFTHSM
 
 	CClientAcceptor acceptor;
