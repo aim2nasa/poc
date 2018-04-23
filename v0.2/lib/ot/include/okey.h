26900adcd429f478d190d15ae91253330b27ac15
Rossi <rossi.kwak@gmail.com>
2018-04-23 08:33:14 +0000

    remove v0.2.1 library and replace it with new folder structure(include,lib)

---
+++ v0.2/lib/ot/include/okey.h
@@ -0,0 +1,66 @@
+#ifndef __OKEY__
+#define __OKEY__
+
+#include <tee_client_api.h>
+
+#define TEE_STORAGE_PRIVATE			0x00000001
+#define TEE_AES_BLOCK_SIZE			16UL
+#define TEE_DATA_FLAG_ACCESS_READ		0x00000001
+#define TEE_DATA_FLAG_ACCESS_WRITE		0x00000002
+#define TEE_DATA_FLAG_ACCESS_WRITE_META		0x00000004
+#define TEE_DATA_FLAG_SHARE_READ		0x00000010
+#define TEE_DATA_FLAG_SHARE_WRITE		0x00000020
+#define TEE_DATA_FLAG_OVERWRITE			0x00000400
+
+typedef void* OperationHandle;
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+	typedef struct _optee_key_context {
+		TEEC_Context *ctx;
+		TEEC_Session *session;
+		uint32_t error;
+		size_t shMemSize;
+	} okey;
+
+	typedef struct _enum_object {
+		uint8_t *id;
+		uint32_t idSize;
+	} eObj;
+
+	typedef struct _enum_object_list {
+		eObj *object;
+		struct _enum_object_list *next;
+	} eObjList;
+
+	TEEC_Result initializeContext(const char *name,okey *o);
+	TEEC_Result openSession(okey *o,uint32_t connectionMethod,
+				const void *connectionData,TEEC_Operation *operation);
+	TEEC_Result keyGen(okey *o,uint32_t storageId,const char *keyFileName,uint32_t flags);
+	TEEC_Result keyOpen(okey *o,uint32_t storageId,const char *keyFileName,uint32_t flags,uint32_t *keyObj);
+	TEEC_Result keyInject(okey *o,uint32_t storageId,const char *keyFileName,uint8_t *keyBuffer,size_t keySize,uint32_t flags);
+	TEEC_Result keyGetObjectBufferAttribute(okey *o,uint32_t keyObj,uint32_t attrId,void *buffer,size_t *bufferSize);
+	TEEC_Result keyEnumObjectList(okey *o,uint32_t storageId,eObjList **list);
+	int keyFreeEnumObjectList(eObjList *list);
+	TEEC_Result keyAllocOper(okey *o,bool bEnc,size_t keySize,OperationHandle *encOp);
+	TEEC_Result keyFreeOper(okey *o,OperationHandle encOp);
+	TEEC_Result keySetkeyOper(okey *o,OperationHandle encOp,uint32_t keyObj);
+	TEEC_Result keyClose(okey *o,uint32_t keyObj);
+	TEEC_Result keyUnlink(okey *o,uint32_t keyObj);
+	TEEC_Result allocShm(okey *o,TEEC_SharedMemory *shm,size_t size);
+	void freeShm(TEEC_SharedMemory *shm);
+	TEEC_Result cipherInit(okey *o,OperationHandle encOp,uint8_t shMemFactor);
+	TEEC_Result cipherUpdate(okey *o,OperationHandle encOp,uint8_t *inBuf,size_t inBufSize);
+	TEEC_Result cipherDoFinal(okey *o,OperationHandle encOp,uint8_t *inBuf,size_t inBufSize);
+	void cipherClose();
+	TEEC_SharedMemory *outSharedMemory();
+	void closeSession(okey *o);
+	void finalizeContext(okey *o);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
