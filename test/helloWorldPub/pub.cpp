735ed2e91b3c8074aa4dd0034213faf4c75d275f
rossi.kwak <rossi.kwak@gmail.com>
2018-11-01 15:02:05 +0900

    domainParticipant 라이브러리 호출

--- test/helloWorldPub/pub.cpp
+++ test/helloWorldPub/pub.cpp
@@ -3,6 +3,12 @@
 
 int main(int argc, char* argv[])
 {
+	if (argc<2) {
+		std::cout << "usage: HelloWorldPub <message>" << std::endl;
+		return -1;
+	}
+	dds::domain::DomainParticipant dp(0);
+
 	std::cout << "end of pub" << std::endl;
 	return 0;
 }
