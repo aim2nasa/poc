779af9d141b6dc9bc19ec0dbe268965e1734145b
Rossi Kwak <rossi.kwak@gmail.com>
2018-11-08 13:36:54 +0900

    copy opensplice dds helloWorld example

---
+++ test/helloWorld/HelloWorldPub.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <gen/HelloWorldData_DCPS.hpp>
+#include <util.h>
+
+int main(int argc,char* argv[])
+{
+  if(argc<2) {
+    std::cout<<"usage: HelloWorldPub <message>"<<std::endl;
+    return -1;
+  }
+
+  dds::domain::DomainParticipant dp(0);
+  dds::topic::Topic<HelloWorldData::Msg> topic(dp,"Message");
+  dds::pub::Publisher pub(dp);
+  dds::pub::DataWriter<HelloWorldData::Msg> dw(pub,topic);
+
+  HelloWorldData::Msg message(1,argv[1]);
+  dw.write(message);
+  std::cout<<"DW: "<<message<<std::endl;
+
+  std::cout<<"pub end"<<std::endl;
+  return 0;
+}
