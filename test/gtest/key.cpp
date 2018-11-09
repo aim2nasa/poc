17eff27a77ee8b69d5f6127f63876860a8bc982d
Rossi Kwak <rossi.kwak@gmail.com>
2018-11-09 13:42:30 +0900

    refactoring human class to remove duplicated code and create encryption,decryption test case for it

--- test/gtest/key.cpp
+++ test/gtest/key.cpp
@@ -17,5 +17,18 @@ TEST(keyStoreTest, basic) {
 
 TEST(humanTest, basic) { 
 	Human h;
-	ASSERT_EQ(0,0);
+
+	h.size_=32;
+	h.key_ = new byte[h.size_];
+	memset(h.key_,0,h.size_);
+
+	std::string plainText = "AES ECB Test";
+	CryptoPP::ECB_Mode<CryptoPP::AES>::Encryption e;
+	std::string cipherText = h.transform(e,plainText);
+
+	ASSERT_NE(plainText,cipherText);
+
+	CryptoPP::ECB_Mode<CryptoPP::AES>::Decryption d;
+	std::string recoveredText = h.transform(d,cipherText);
+	ASSERT_EQ(plainText,recoveredText);
 }
