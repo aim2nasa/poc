dc5b0a19d67236fbceaccc0dc803b68d9b7ecc2c
Rossi Kwak <rossi.kwak@gmail.com>
2018-11-12 14:58:23 +0900

    implement decrypt function and test case for it

--- test/gtest/entity/node.h
+++ test/gtest/entity/node.h
@@ -7,6 +7,11 @@
 #include <cryptopp/filters.h>
 #include <cryptopp/ccm.h>
 
+#define DECRYPT_OK			 			 0	
+#define FAIL_GET_LAST_RESULT 			-1	
+#define ERROR_TAG_SIZE_NOT_MATCH 	-100
+#define ERROR_WRONG_INPUT_TAG_SIZE	-101
+
 class Node : public KeyStore{
 public:
 	Node(){ memset(iv_,0,sizeof(iv_)); }
@@ -34,6 +39,35 @@ public:
 		ef.ChannelPut("",(const byte*)input.data(),input.size());
 		ef.ChannelMessageEnd("");
 		return output;
+	}
+
+	template <typename T>
+	int decrypt(T d,int tagSize,std::string aad,std::string input,std::string& output) {
+		std::string enc = input.substr(0,input.size()-tagSize);
+		std::string tag = input.substr(input.size()-tagSize);
+
+		if(input.size()!=(enc.size()+tag.size())) return ERROR_TAG_SIZE_NOT_MATCH;
+		if(tag.size()!=tagSize) return ERROR_WRONG_INPUT_TAG_SIZE;
+
+		d.SpecifyDataLengths(aad.size(),enc.size(),0);
+
+		CryptoPP::AuthenticatedDecryptionFilter df(d,NULL,
+			CryptoPP::AuthenticatedDecryptionFilter::THROW_EXCEPTION);
+
+		df.ChannelPut("AAD",(const byte*)aad.data(),aad.size());
+		df.ChannelMessageEnd("AAD");
+
+		df.ChannelPut("",(const byte*)enc.data(),enc.size());
+		df.ChannelPut("",(const byte*)tag.data(),tag.size());
+		df.ChannelMessageEnd("");
+
+		if(!df.GetLastResult()) return FAIL_GET_LAST_RESULT;
+
+		df.SetRetrievalChannel("");
+		output.resize((size_t)df.MaxRetrievable());
+		df.Get( (byte*)output.data(),output.size());
+
+		return DECRYPT_OK; 
 	}
 
 	byte iv_[CryptoPP::AES::BLOCKSIZE];
