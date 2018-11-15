baaef013ba6585305eec43ac2a2175f230fab9d3
Rossi Kwak <rossi.kwak@gmail.com>
2018-11-15 12:51:35 +0900

    remove needless indentations

--- test/gtest/hacking.cpp
+++ test/gtest/hacking.cpp
@@ -23,21 +23,19 @@ void AliceToBob(Node &Alice,Node &Bob,int tagSize,std::string &cipherText)
 	ASSERT_EQ(memcmp(Alice.iv_,Bob.iv_,CryptoPP::AES::BLOCKSIZE),0);
 
 	//Secret message fron Alice to Bob
-	{
-		//Encryption from Alice
-		std::string message = "Hello Bob";
-		CryptoPP::GCM<CryptoPP::AES>::Encryption e;
-		e.SetKeyWithIV(Alice.key_,Alice.size_,Alice.iv_);
-		cipherText = Alice.encrypt(e,"AAD",message,tagSize);
-		ASSERT_NE(message,cipherText);
+	//Encryption from Alice
+	std::string message = "Hello Bob";
+	CryptoPP::GCM<CryptoPP::AES>::Encryption e;
+	e.SetKeyWithIV(Alice.key_,Alice.size_,Alice.iv_);
+	cipherText = Alice.encrypt(e,"AAD",message,tagSize);
+	ASSERT_NE(message,cipherText);
 
-		//Decryption from Bob
-		CryptoPP::GCM<CryptoPP::AES>::Decryption d;
-		d.SetKeyWithIV(Bob.key_,Bob.size_,Bob.iv_);
-		std::string recoveredText;
-		ASSERT_EQ(Bob.decrypt(d,tagSize,"AAD",cipherText,recoveredText),DECRYPT_OK);
-		ASSERT_EQ(recoveredText,message);
-	}
+	//Decryption from Bob
+	CryptoPP::GCM<CryptoPP::AES>::Decryption d;
+	d.SetKeyWithIV(Bob.key_,Bob.size_,Bob.iv_);
+	std::string recoveredText;
+	ASSERT_EQ(Bob.decrypt(d,tagSize,"AAD",cipherText,recoveredText),DECRYPT_OK);
+	ASSERT_EQ(recoveredText,message);
 }
  
 TEST(hackingTest, unauthorised_Subscription) { 
@@ -51,21 +49,19 @@ TEST(hackingTest, unauthorised_Subscription) {
 	AliceToBob(Alice,Bob,tagSize,cipherText);
 
 	//Eve, doesn't have the key Alice and Bob share
-	{
-		Node Eve;
-		Eve.size_ = 32;
-		Eve.key_ = new byte[Eve.size_];
-		ASSERT_NE(memcmp(Eve.key_,Alice.key_,Eve.size_),0);
-		ASSERT_NE(memcmp(Eve.iv_,Alice.iv_,CryptoPP::AES::BLOCKSIZE),0);
+	Node Eve;
+	Eve.size_ = 32;
+	Eve.key_ = new byte[Eve.size_];
+	ASSERT_NE(memcmp(Eve.key_,Alice.key_,Eve.size_),0);
+	ASSERT_NE(memcmp(Eve.iv_,Alice.iv_,CryptoPP::AES::BLOCKSIZE),0);
 
-		//Eavesdrop Alice's encrypted message(i.e. cipherText)
-		//Decryption has to end up fail because Eve doesn't have key,iv
-		CryptoPP::GCM<CryptoPP::AES>::Decryption d;
-		d.SetKeyWithIV(Eve.key_,Eve.size_,Eve.iv_);
-		std::string recoveredText;
-		ASSERT_EQ(Eve.decrypt(d,tagSize,"AAD",cipherText,recoveredText),ERROR_HASH_VERIFY_FAILED);
-		ASSERT_EQ(recoveredText.size(),0);
-	}
+	//Eavesdrop Alice's encrypted message(i.e. cipherText)
+	//Decryption has to end up fail because Eve doesn't have key,iv
+	CryptoPP::GCM<CryptoPP::AES>::Decryption d;
+	d.SetKeyWithIV(Eve.key_,Eve.size_,Eve.iv_);
+	std::string recoveredText;
+	ASSERT_EQ(Eve.decrypt(d,tagSize,"AAD",cipherText,recoveredText),ERROR_HASH_VERIFY_FAILED);
+	ASSERT_EQ(recoveredText.size(),0);
 }
 
 TEST(hackingTest, unauthorised_Publication) { 
@@ -77,26 +73,24 @@ TEST(hackingTest, unauthorised_Publication) {
 	AliceToBob(Alice,Bob,tagSize,cipherText);
 
 	//Trudy, doesn't have the key Alice and Bob share(No authorization)
-	{
-		Node Trudy;
-		Trudy.size_ = 32;
-		Trudy.key_ = new byte[Trudy.size_];
-		ASSERT_NE(memcmp(Trudy.key_,Alice.key_,Trudy.size_),0);
-		ASSERT_NE(memcmp(Trudy.iv_,Alice.iv_,CryptoPP::AES::BLOCKSIZE),0);
+	Node Trudy;
+	Trudy.size_ = 32;
+	Trudy.key_ = new byte[Trudy.size_];
+	ASSERT_NE(memcmp(Trudy.key_,Alice.key_,Trudy.size_),0);
+	ASSERT_NE(memcmp(Trudy.iv_,Alice.iv_,CryptoPP::AES::BLOCKSIZE),0);
 
-		//Trudy creates manipulated message to send to Bob as if it comes from Alice
-		std::string trudyMessage = "Bob you idiot";
-		CryptoPP::GCM<CryptoPP::AES>::Encryption e;
-		e.SetKeyWithIV(Trudy.key_,Trudy.size_,Trudy.iv_);
-		cipherText = Trudy.encrypt(e,"AAD",trudyMessage,tagSize);
-		ASSERT_NE(trudyMessage,cipherText);
+	//Trudy creates manipulated message to send to Bob as if it comes from Alice
+	std::string trudyMessage = "Bob you idiot";
+	CryptoPP::GCM<CryptoPP::AES>::Encryption e;
+	e.SetKeyWithIV(Trudy.key_,Trudy.size_,Trudy.iv_);
+	cipherText = Trudy.encrypt(e,"AAD",trudyMessage,tagSize);
+	ASSERT_NE(trudyMessage,cipherText);
 
-		//Bob receives message from Trudy
-		//Through MAC validation, Bob can find out the message is from the one who has key or not. 
-		CryptoPP::GCM<CryptoPP::AES>::Decryption d;
-		d.SetKeyWithIV(Bob.key_,Bob.size_,Bob.iv_);
-		std::string recoveredText;
-		ASSERT_EQ(Bob.decrypt(d,tagSize,"AAD",cipherText,recoveredText),ERROR_HASH_VERIFY_FAILED);
-		ASSERT_EQ(recoveredText.size(),0);
-	}
+	//Bob receives message from Trudy
+	//Through MAC validation, Bob can find out the message is from the one who has key or not. 
+	CryptoPP::GCM<CryptoPP::AES>::Decryption d;
+	d.SetKeyWithIV(Bob.key_,Bob.size_,Bob.iv_);
+	std::string recoveredText;
+	ASSERT_EQ(Bob.decrypt(d,tagSize,"AAD",cipherText,recoveredText),ERROR_HASH_VERIFY_FAILED);
+	ASSERT_EQ(recoveredText.size(),0);
 }
