90177639ede277f72e41dfb3bea273bd40d75b00
Rossi Kwak <rossi.kwak@gmail.com>
2018-11-15 12:49:42 +0900

    refactoring, extract duplicated codes into one function

--- test/gtest/hacking.cpp
+++ test/gtest/hacking.cpp
@@ -1,22 +1,19 @@
 #include <gtest/gtest.h>
 #include <entity/keyStore.h>
 #include <entity/node.h>
- 
-TEST(hackingTest, unauthorised_Subscription) { 
-	ASSERT_EQ(16,sizeof(Node::iv_));
 
-	//Alice
-	Node Alice;
+void AliceToBob(Node &Alice,Node &Bob,int tagSize,std::string &cipherText)
+{
 	int kv = 3; //ranom value
 	int ivv = 4; //ranom value
 
+	//Alice
 	Alice.size_=32;
 	Alice.key_ = new byte[Alice.size_];
 	memset(Alice.key_,kv,Alice.size_);
 	memset(Alice.iv_,ivv,CryptoPP::AES::BLOCKSIZE);
 
 	//Bob, Alice and Bob share the same key and iv
-	Node Bob;
 	Bob.size_ = Alice.size_;
 	Bob.key_ = new byte[Bob.size_];
 	memset(Bob.key_,kv,Bob.size_);
@@ -25,8 +22,6 @@ TEST(hackingTest, unauthorised_Subscription) {
 	ASSERT_EQ(memcmp(Alice.key_,Bob.key_,Alice.size_),0);
 	ASSERT_EQ(memcmp(Alice.iv_,Bob.iv_,CryptoPP::AES::BLOCKSIZE),0);
 
-	int tagSize = 16;
-	std::string cipherText;
 	//Secret message fron Alice to Bob
 	{
 		//Encryption from Alice
@@ -43,6 +38,17 @@ TEST(hackingTest, unauthorised_Subscription) {
 		ASSERT_EQ(Bob.decrypt(d,tagSize,"AAD",cipherText,recoveredText),DECRYPT_OK);
 		ASSERT_EQ(recoveredText,message);
 	}
+}
+ 
+TEST(hackingTest, unauthorised_Subscription) { 
+	ASSERT_EQ(16,sizeof(Node::iv_));
+
+	//Alice and Bob
+	Node Alice,Bob;
+
+	int tagSize = 16;
+	std::string cipherText;
+	AliceToBob(Alice,Bob,tagSize,cipherText);
 
 	//Eve, doesn't have the key Alice and Bob share
 	{
@@ -63,44 +69,12 @@ TEST(hackingTest, unauthorised_Subscription) {
 }
 
 TEST(hackingTest, unauthorised_Publication) { 
-	//Alice
-	Node Alice;
-	int kv = 3; //ranom value
-	int ivv = 4; //ranom value
-
-	Alice.size_=32;
-	Alice.key_ = new byte[Alice.size_];
-	memset(Alice.key_,kv,Alice.size_);
-	memset(Alice.iv_,ivv,CryptoPP::AES::BLOCKSIZE);
-
-	//Bob, Alice and Bob share the same key and iv
-	Node Bob;
-	Bob.size_ = Alice.size_;
-	Bob.key_ = new byte[Bob.size_];
-	memset(Bob.key_,kv,Bob.size_);
-	memset(Bob.iv_,ivv,CryptoPP::AES::BLOCKSIZE);
-	ASSERT_EQ(Alice.size_,Bob.size_);
-	ASSERT_EQ(memcmp(Alice.key_,Bob.key_,Alice.size_),0);
-	ASSERT_EQ(memcmp(Alice.iv_,Bob.iv_,CryptoPP::AES::BLOCKSIZE),0);
+	//Alice and Bob
+	Node Alice,Bob;
 
 	int tagSize = 16;
 	std::string cipherText;
-	//Secret message fron Alice to Bob
-	{
-		//Encryption from Alice
-		std::string message = "Hello Bob";
-		CryptoPP::GCM<CryptoPP::AES>::Encryption e;
-		e.SetKeyWithIV(Alice.key_,Alice.size_,Alice.iv_);
-		cipherText = Alice.encrypt(e,"AAD",message,tagSize);
-		ASSERT_NE(message,cipherText);
-
-		//Decryption from Bob
-		CryptoPP::GCM<CryptoPP::AES>::Decryption d;
-		d.SetKeyWithIV(Bob.key_,Bob.size_,Bob.iv_);
-		std::string recoveredText;
-		ASSERT_EQ(Bob.decrypt(d,tagSize,"AAD",cipherText,recoveredText),DECRYPT_OK);
-		ASSERT_EQ(recoveredText,message);
-	}
+	AliceToBob(Alice,Bob,tagSize,cipherText);
 
 	//Trudy, doesn't have the key Alice and Bob share(No authorization)
 	{
