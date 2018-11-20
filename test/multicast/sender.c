71e352bdf19bd3ecb7a93e6b95c1e0cfa49bdfac
Rossi Kwak <rossi.kwak@gmail.com>
2018-11-20 16:55:22 +0900

    readin commandline user message, and code indentation rearrange

--- test/multicast/sender.c
+++ test/multicast/sender.c
@@ -15,16 +15,19 @@ int main(int argc, char *argv[])
      struct sockaddr_in addr;
      int fd, cnt;
      struct ip_mreq mreq;
-     char *message="Hello, World!";
+     char *message = NULL;
 
-	  if(argc>1) {
-	      message = argv[1];
+     if(argc>1) {
+         message = argv[1];
+         printf("message:%s\n",message);
+     }else{
+         printf("usage: sender <message>\n");
+			return -1;
 	  }
-	  printf("message:%s\n",message);
 
      if ((fd=socket(AF_INET,SOCK_DGRAM,0)) < 0) {
-	      perror("socket");
-	      return -1;
+         perror("socket");
+         return -1;
      }
 
      memset(&addr,0,sizeof(addr));
@@ -33,11 +36,11 @@ int main(int argc, char *argv[])
      addr.sin_port=htons(MULTICAST_PORT);
      
      while (1) {
-	      if (sendto(fd,message,strlen(message),0,(struct sockaddr *) &addr,sizeof(addr)) < 0) {
-	     	    perror("sendto");
-				 return -1;
-	      }
-	      sleep(1);
+         if (sendto(fd,message,strlen(message),0,(struct sockaddr *) &addr,sizeof(addr)) < 0) {
+             perror("sendto");
+             return -1;
+         }
+         sleep(1);
      }
-	  return 0;
+     return 0;
 }
