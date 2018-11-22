1216facb2466c2f2c7d3e2c81d18f1bc08e95566
Rossi Kwak <rossi.kwak@gmail.com>
2018-11-22 17:02:57 +0900

    create enc/dec test cast for #265

---
+++ test/gtest/encoding.cpp
@@ -0,0 +1,30 @@
+#include <gtest/gtest.h>
+#include <entity/keyStore.h>
+#include <entity/node.h>
+
+TEST(encodingTest, bug_NO265) { 
+	Node Alice;
+	Alice.size_ = 32;
+	Alice.key_ = new byte[Alice.size_];
+	memset(Alice.key_,0,Alice.size_);
+	memset(Alice.iv_,0,CryptoPP::AES::BLOCKSIZE);
+
+	std::string message = "Hellocrazy";
+
+	int tagSize = 16;
+	CryptoPP::GCM<CryptoPP::AES>::Encryption e;
+	e.SetKeyWithIV(Alice.key_,Alice.size_,Alice.iv_);
+	std::string cipherText = Alice.encrypt(e,"AAD",message,tagSize);
+
+	CryptoPP::GCM<CryptoPP::AES>::Decryption d;
+	d.SetKeyWithIV(Alice.key_,Alice.size_,Alice.iv_);
+	std::string recoveredText;
+	ASSERT_EQ(Alice.decrypt(d,tagSize,"AAD",cipherText,recoveredText),DECRYPT_OK);
+	ASSERT_EQ(message,recoveredText);
+
+	message = "HelloCrazy";
+
+	cipherText = Alice.encrypt(e,"AAD",message,tagSize);
+	ASSERT_EQ(Alice.decrypt(d,tagSize,"AAD",cipherText,recoveredText),DECRYPT_OK);
+	ASSERT_EQ(message,recoveredText);
+}
