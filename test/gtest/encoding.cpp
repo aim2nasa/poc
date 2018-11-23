6fdf60373131775f0641a8fccb5fb3c7ab0b7368
Rossi Kwak <rossi.kwak@gmail.com>
2018-11-23 17:11:44 +0900

    #265, modify test case

--- test/gtest/encoding.cpp
+++ test/gtest/encoding.cpp
@@ -9,22 +9,23 @@ TEST(encodingTest, bug_NO265) {
 	memset(Alice.key_,0,Alice.size_);
 	memset(Alice.iv_,0,CryptoPP::AES::BLOCKSIZE);
 
+	std::string adata(16, (char)0x00);
 	std::string message = "Hellocrazy";
 
 	int tagSize = 16;
 	CryptoPP::GCM<CryptoPP::AES>::Encryption e;
 	e.SetKeyWithIV(Alice.key_,Alice.size_,Alice.iv_);
-	std::string cipherText = Alice.encrypt(e,"AAD",message,tagSize);
+	std::string cipherText = Alice.encrypt(e,adata,message,tagSize);
 
 	CryptoPP::GCM<CryptoPP::AES>::Decryption d;
 	d.SetKeyWithIV(Alice.key_,Alice.size_,Alice.iv_);
 	std::string recoveredText;
-	ASSERT_EQ(Alice.decrypt(d,tagSize,"AAD",cipherText,recoveredText),DECRYPT_OK);
+	ASSERT_EQ(Alice.decrypt(d,tagSize,adata,cipherText,recoveredText),DECRYPT_OK);
 	ASSERT_EQ(message,recoveredText);
 
 	message = "HelloCrazy";
 
-	cipherText = Alice.encrypt(e,"AAD",message,tagSize);
-	ASSERT_EQ(Alice.decrypt(d,tagSize,"AAD",cipherText,recoveredText),DECRYPT_OK);
+	cipherText = Alice.encrypt(e,adata,message,tagSize);
+	ASSERT_EQ(Alice.decrypt(d,tagSize,adata,cipherText,recoveredText),DECRYPT_OK);
 	ASSERT_EQ(message,recoveredText);
 }
