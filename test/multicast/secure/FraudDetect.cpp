0ef8397656f172bd50a9222461b64b24b894ad40
Rossi Kwak <rossi.kwak@gmail.com>
2018-11-30 15:00:01 +0900

    define init function

--- test/multicast/secure/FraudDetect.cpp
+++ test/multicast/secure/FraudDetect.cpp
@@ -1,4 +1,7 @@
 #include "FraudDetect.h"
+#include <sys/socket.h>
+#include <netinet/ip.h>
+#include <string.h>
 
 FraudDetect::FraudDetect()
 {
@@ -6,4 +9,22 @@ FraudDetect::FraudDetect()
 
 FraudDetect::~FraudDetect()
 {
+}
+
+int FraudDetect::init(int port,int backlog)
+{
+    if((sock_ = socket(AF_INET, SOCK_STREAM, 0))<0){
+        return ERROR_SOCKET;
+    }
+
+    struct sockaddr_in addr;
+    memset(&addr, 0x00, sizeof(addr));
+    addr.sin_family = AF_INET;
+    addr.sin_addr.s_addr = htonl(INADDR_ANY);
+    addr.sin_port = htons(port);
+
+    if(bind(sock_, (struct sockaddr *)&addr, sizeof(addr)) < 0) return ERROR_BIND;
+    if(listen(sock_,backlog) < 0) return ERROR_LISTEN;
+
+    return OK;
 }
--- test/multicast/secure/FraudDetect.h
+++ test/multicast/secure/FraudDetect.h
@@ -1,10 +1,19 @@
 #ifndef __FRAUDDETECT_H__
 #define __FRAUDDETECT_H__
 
+#define OK              0
+#define ERROR_SOCKET    -10
+#define ERROR_BIND      -10
+#define ERROR_LISTEN    -10
+
 class FraudDetect{
 public:
 	FraudDetect();
 	~FraudDetect();
+
+    int init(int port,int backlog=1);
+
+    int sock_;
 };
 
 #endif
