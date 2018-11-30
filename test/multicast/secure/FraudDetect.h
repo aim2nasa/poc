ab37f4e306bbf4d00fc88dae4dbbbbdbe3b77a49
Rossi Kwak <rossi.kwak@gmail.com>
2018-11-30 15:27:56 +0900

    bug fix, error code

--- test/multicast/secure/FraudDetect.h
+++ test/multicast/secure/FraudDetect.h
@@ -3,9 +3,9 @@
 
 #define OK              0
 #define ERROR_SOCKET    -10
-#define ERROR_BIND      -10
-#define ERROR_LISTEN    -10
-#define ERROR_RECEIVE   -10
+#define ERROR_BIND      -11
+#define ERROR_LISTEN    -12
+#define ERROR_RECEIVE   -13
 
 class FraudDetect{
 public:
