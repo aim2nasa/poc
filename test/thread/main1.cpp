db7c84be69219a5255c8a5c4df17c799332c18b3
Rossi Kwak <rossi.kwak@gmail.com>
2018-12-11 20:41:36 +0900

    create and join thread and link with pthread library

--- test/thread/main1.cpp
+++ test/thread/main1.cpp
@@ -2,6 +2,11 @@
 
 pthread_mutex_t mtx;
 
+void *run(void *arg)
+{
+    
+}
+
 int main(int argc, char *argv[])
 {
     int rtn = pthread_mutex_init(&mtx,NULL);
@@ -10,6 +15,17 @@ int main(int argc, char *argv[])
         return -1;
     }
     std::cout<<"mutex init ok"<<std::endl;
+
+    pthread_t t[2];
+    char *name[]={ (char*)"Thread1",(char*)"Thread2"};
+    for(int i=0;i<sizeof(t)/sizeof(pthread_t);i++)
+        pthread_create(&t[i],NULL,run,(void *)name[i]);
+
+    int status;
+    for(int i=0;i<sizeof(t)/sizeof(pthread_t);i++){
+        pthread_join(t[i],(void**)&status);
+        std::cout<<name[i]<<", join status="<<status<<std::endl;
+    }
 
     pthread_mutex_destroy(&mtx);
     std::cout<<"end of main"<<std::endl;
