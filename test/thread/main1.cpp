f442e228e92c94d9c74f579f0021f80b4577f486
Rossi Kwak <rossi.kwak@gmail.com>
2018-12-11 20:21:14 +0900

    mutex initialize

--- test/thread/main1.cpp
+++ test/thread/main1.cpp
@@ -1,7 +1,17 @@
 #include <iostream>
 
+pthread_mutex_t mtx;
+
 int main(int argc, char *argv[])
 {
+    int rtn = pthread_mutex_init(&mtx,NULL);
+    if(rtn) {
+        std::cout<<"mutex init fail("<<rtn<<")"<<std::endl;
+        return -1;
+    }
+    std::cout<<"mutex init ok"<<std::endl;
+
+    pthread_mutex_destroy(&mtx);
     std::cout<<"end of main"<<std::endl;
     return 0;
 }
