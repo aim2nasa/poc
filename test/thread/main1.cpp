9b1ae33979e97bd34c63558e5a833481c9552319
Rossi Kwak <rossi.kwak@gmail.com>
2018-12-11 22:40:30 +0900

    apply thread lock when LOCK is defined, increate threads to 40 to exaggerate data corruption when no synchronization applied

--- test/thread/main1.cpp
+++ test/thread/main1.cpp
@@ -1,10 +1,41 @@
 #include <iostream>
+#include <unistd.h>
+#include <stdio.h>
+
+#define MAX 10000
 
 pthread_mutex_t mtx;
+int number=0;
 
-void *run(void *arg)
+void *add(void *arg)
 {
-    std::cout<<"name:"<<(char*)arg<<std::endl;
+    for(int i=0;i<MAX;i++) {
+#ifdef LOCK
+        pthread_mutex_lock(&mtx);
+#endif
+        number++;
+        std::cout<<(char*)arg<<",number:"<<number<<std::endl;
+#ifdef LOCK
+        pthread_mutex_unlock(&mtx);
+#endif
+        usleep(0);
+    }
+    return 0;
+}
+
+void *del(void *arg)
+{
+    for(int i=0;i<MAX;i++) {
+#ifdef LOCK
+        pthread_mutex_lock(&mtx);
+#endif
+        number--;
+        std::cout<<(char*)arg<<",number:"<<number<<std::endl;
+#ifdef LOCK
+        pthread_mutex_unlock(&mtx);
+#endif
+        usleep(1);
+    }
     return 0;
 }
 
@@ -17,15 +48,23 @@ int main(int argc, char *argv[])
     }
     std::cout<<"mutex init ok"<<std::endl;
 
-    pthread_t t[2];
-    char *name[]={ (char*)"Thread1",(char*)"Thread2"};
-    for(int i=0;i<sizeof(t)/sizeof(pthread_t);i++)
-        pthread_create(&t[i],NULL,run,(void *)name[i]);
+    pthread_t t[40];
+    char* name[]={
+                    "t01","t02","t03","t04","t05","t06","t07","t08","t09","t10",
+                    "t11","t12","t13","t14","t15","t16","t17","t18","t19","t20",
+                    "t21","t22","t23","t24","t25","t26","t27","t28","t29","t30",
+                    "t31","t32","t33","t34","t35","t36","t37","t38","t39","t40",
+    };
+
+    void *(*runs[])(void *)={ add,del };
+    for(int i=0;i<sizeof(t)/sizeof(pthread_t);i++){
+        pthread_create(&t[i],NULL,runs[i%2],(void *)name[i]);
+    }
 
     int status;
     for(int i=0;i<sizeof(t)/sizeof(pthread_t);i++){
         pthread_join(t[i],(void**)&status);
-        std::cout<<name[i]<<", join status="<<status<<std::endl;
+//      std::cout<<name[i]<<", join status="<<status<<std::endl;
     }
 
     pthread_mutex_destroy(&mtx);
