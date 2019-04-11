606e21314e00e237c21498960981d1f5d9fe422b
Rossi Kwak <rossi.kwak@gmail.com>
2019-04-11 17:31:26 +0900

    check the vector size after buffer added

--- test/wc2/mcnet.cpp
+++ test/wc2/mcnet.cpp
@@ -8,11 +8,12 @@ TEST(IMcNetTest, send) {
     public:
         ssize_t send(const void *buf, size_t len)
         {
-            std::vector<byte> b;
+            std::vector<byte> b((byte*)buf,(byte*)buf+len);
             return b.size();
         }
     };
 
+	ASSERT_EQ(sizeof("abc"), 4);
     CTest t;
-	ASSERT_EQ(t.send("abc",sizeof("abc")), 3);
+	ASSERT_EQ(t.send("abc",3), 3);
 }
