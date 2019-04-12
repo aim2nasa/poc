927a0494396192033a0f0ef5135f7f29ece2a4fa
Rossi Kwak <rossi.kwak@gmail.com>
2019-04-12 12:59:31 +0900

    create simple sendRecv test case

--- test/wc2/mcnet.cpp
+++ test/wc2/mcnet.cpp
@@ -18,3 +18,37 @@ TEST(IMcNetTest, send) {
     CTest t;
 	ASSERT_EQ(t.send("abc",3), 3);
 }
+
+TEST(IMcNetTest, sendRecv) { 
+    typedef unsigned char byte;
+
+    class CSender : public IMcNet{
+    public:
+        int join(IRecv *rcv)
+        {
+            rcv_ = rcv;
+            return 0;
+        }
+        ssize_t send(const void *buf, size_t len)
+        {
+            std::vector<byte> b((byte*)buf,(byte*)buf+len);
+            rcv_->recv(b.data(),b.size());
+            return b.size();
+        }
+        IRecv *rcv_;
+    };
+
+    class CReceiver : public IRecv{
+    public:
+        ssize_t recv(void *buf,size_t len)
+        {
+            std::cout<<"CReceiver::recv="<<len<<std::endl;
+            return len;
+        }
+    }; 
+
+    CReceiver r;
+    CSender s; 
+    ASSERT_EQ(s.join(&r),0);
+    ASSERT_EQ(s.send("abc",3),3);
+}
