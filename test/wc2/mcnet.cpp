0ee5736d0b3c967202ee525406cd9ad33497d8c4
Rossi Kwak <rossi.kwak@gmail.com>
2019-04-12 15:39:39 +0900

    create testcase on multiple receivers

--- test/wc2/mcnet.cpp
+++ test/wc2/mcnet.cpp
@@ -81,3 +81,50 @@ TEST(IMcNetTest, set) {
 
     ASSERT_EQ(tb.size(),3);
 }
+
+TEST(IMcNetTest, multiReceivers)
+{
+    typedef unsigned char byte;
+
+    class CSender : public IMcNet{
+    public:
+        int join(IRecv *rcv)
+        {
+            std::pair<std::set<IRecv*>::iterator,bool> ret = tb_.insert(rcv);
+            if(ret.second==false) return -1;
+            return 0;
+        }
+        ssize_t send(const void *buf, size_t len)
+        {
+            std::vector<byte> b((byte*)buf,(byte*)buf+len);
+            for(std::set<IRecv*>::iterator it=tb_.begin();it!=tb_.end();++it)
+                (*it)->recv(b.data(),b.size());
+                
+            return b.size();
+        }
+        std::set<IRecv*> tb_;
+    };
+
+    class CReceiver : public IRecv{
+    public:
+        CReceiver(std::string name):name_(name){}
+        ssize_t recv(void *buf,size_t len)
+        {
+            std::cout<<"CReceiver("<<name_<<")::recv="<<len<<std::endl;
+            for(size_t i=0;i<len;i++) std::cout<<*(reinterpret_cast<char*>(buf)+i);
+            std::cout<<std::endl;
+            return len;
+        }
+        std::string name_;
+    }; 
+
+    CReceiver r1("R1"),r2("R2"),r3("R3");
+    CSender s;
+    ASSERT_EQ(s.join(&r1),0);
+    ASSERT_EQ(s.join(&r1),-1);  //already exist
+    ASSERT_EQ(s.join(&r2),0);
+    ASSERT_EQ(s.join(&r3),0);
+    ASSERT_EQ(s.tb_.size(),3);
+
+    ASSERT_EQ(s.send("abc",3),3);
+}
