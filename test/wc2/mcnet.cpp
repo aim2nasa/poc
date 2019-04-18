90470d61c0091bf66c3cec2d92ea1cfb1b1bd648
Rossi Kwak <rossi.kwak@gmail.com>
2019-04-18 16:34:07 +0900

    Design upgrade of 'realnetDesignExperiment1' by introducing ISession,ISender,IReceiver interfaces

--- test/wc2/mcnet.cpp
+++ test/wc2/mcnet.cpp
@@ -458,3 +458,95 @@ TEST(MockTest, realnetDesignExperiment1)
     ASSERT_EQ(ri->recv(buf,sizeof(buf)),len);
     ASSERT_EQ(memcmp(buf,msg,len),0);
 }
+
+struct ISession{
+    virtual ~ISession() {}
+    virtual int init(const char *ip,ushort port)=0;
+    virtual int close()=0;
+};
+
+struct ISender : public ISession{
+    virtual ~ISender() {}
+    virtual ssize_t send(const void *buf,size_t len)=0;
+};
+
+struct IReceiver : public ISession{
+    virtual ~IReceiver() {}
+    virtual ssize_t recv(void *buf,size_t len)=0;
+};
+
+TEST(MockTest, realnetDesignExperiment2)
+{
+    class Sender : public ISender{
+    public:
+        Sender():fd_(-1){}
+        virtual int init(const char *ip,ushort port) {
+            if((fd_=socket(AF_INET,SOCK_DGRAM,0)) < 0) return -1;
+
+            memset(&addr_,0,sizeof(addr_));
+            addr_.sin_family = AF_INET;
+            addr_.sin_addr.s_addr = inet_addr(ip);
+            addr_.sin_port=port;
+            return 0;
+        }
+        virtual ssize_t send(const void *buf,size_t len)
+        {
+            return sendto(fd_,buf,len,0,(struct sockaddr *) &addr_,sizeof(addr_)); 
+        }
+        virtual int close() { return ::close(fd_); }
+    private:
+        int fd_;
+        struct sockaddr_in addr_;
+    };
+
+    class Receiver : public IReceiver{
+    public:
+        Receiver(int enable=1):fd_(-1),enable_(enable){}
+        virtual int init(const char *ip,ushort port) {
+            if((fd_=socket(AF_INET,SOCK_DGRAM,0)) < 0) return -1;
+            if(setsockopt(fd_,SOL_SOCKET,SO_REUSEADDR,&enable_,sizeof(enable_)) < 0) return -2; 
+
+            memset(&addr_,0,sizeof(addr_));
+            addr_.sin_family=AF_INET;
+            addr_.sin_addr.s_addr=htonl(INADDR_ANY);
+            addr_.sin_port=port;
+
+            if(bind(fd_,(struct sockaddr *)&addr_,sizeof(addr_)) < 0) return -3;
+
+            struct ip_mreq mreq;
+            mreq.imr_multiaddr.s_addr=inet_addr(ip);
+            mreq.imr_interface.s_addr=htonl(INADDR_ANY);
+            if(setsockopt(fd_,IPPROTO_IP,IP_ADD_MEMBERSHIP,&mreq,sizeof(mreq)) < 0) return -4;
+
+            return 0;
+        }
+        virtual ssize_t recv(void *buf,size_t len)
+        {
+            socklen_t addrlen = sizeof(addr_);
+            return recvfrom(fd_,buf,len,0,(struct sockaddr *)&addr_,&addrlen);
+        }
+        virtual int close() { return ::close(fd_); }
+    private:
+        int fd_,enable_;
+        struct sockaddr_in addr_;
+    };
+
+    Sender s;
+    Receiver r;
+
+    ISender *si = &s;
+    IReceiver *ri = &r;
+
+    ASSERT_EQ(si->init(MULTICAST_GROUP,MULTICAST_PORT),0);
+    ASSERT_EQ(ri->init(MULTICAST_GROUP,MULTICAST_PORT),0);
+
+    char msg[]={"abcde"};   //message to send
+    size_t len=sizeof(msg); //size of message to send
+    ASSERT_EQ(len,6);
+
+    ASSERT_EQ(si->send(msg,len),len);
+
+    char buf[128];
+    ASSERT_EQ(ri->recv(buf,sizeof(buf)),len);
+    ASSERT_EQ(memcmp(buf,msg,len),0);
+}
