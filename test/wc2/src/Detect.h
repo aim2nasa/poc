a5888960252da266bdbbda6da6f356c6fad2b625
Rossi <rossi.kwak@gmail.com>
2019-04-24 22:05:31 +0900

    Add infinite loop in thread function and devise mechanism which can escape from it

--- test/wc2/src/Detect.h
+++ test/wc2/src/Detect.h
@@ -8,10 +8,16 @@ typedef SafeQueue<std::vector<byte>> Queue;
 
 class Detect : public IReceive{
 public:
-    Detect(){}
+    Detect():tid_(-1),stop_(false){}
     virtual ~Detect(){}
 
     int start(void *arg) { return pthread_create(&tid_,NULL,run,arg); }
+    void stop()
+    {
+        stop_ = true;
+        std::vector<byte> nullMsg;
+        q_.enqueue(nullMsg);
+    }
     int join(void **retval=0) { return pthread_join(tid_,retval); }
     ssize_t recv(void *buf,size_t len)
     {
@@ -24,10 +30,16 @@ public:
 private:
     pthread_t tid_;
     Queue q_;
+    bool stop_;
     static void* run(void *arg)
     {
         Detect *p = static_cast<Detect*>(arg);
         std::cout<<"start run.."<<std::endl;
+        while(1)
+        {
+            std::vector<byte> msg = p->q_.dequeue();
+            if(msg.size()==0 && p->stop_) break;    //Escape from infinite loop 
+        }
         std::cout<<"end of run"<<std::endl;
         return 0;
     }
