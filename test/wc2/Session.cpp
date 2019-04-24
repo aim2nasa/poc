68788e137bd95e40633e5083d3e0ab60ee6601be
Rossi <rossi.kwak@gmail.com>
2019-04-24 14:28:44 +0900

    add 'MultipleOpenClose' testcast

--- test/wc2/Session.cpp
+++ test/wc2/Session.cpp
@@ -25,3 +25,24 @@ TEST(Session, openClose)
     EXPECT_EQ(s.init(0,0),0);
     EXPECT_EQ(s.close(),0);
 }
+
+TEST(Session, MultipleOpenClose)
+{
+    MockSession ms;
+    EXPECT_CALL(ms,init(testing::_,testing::_))
+        .WillOnce(testing::Return(0))
+        .WillRepeatedly(testing::Return(-1));
+
+    EXPECT_CALL(ms,close())
+        .WillOnce(testing::Return(0))
+        .WillRepeatedly(testing::Return(-1));
+
+    Session s(&ms);
+    EXPECT_EQ(s.init(0,0),0);
+    EXPECT_EQ(s.init(0,0),-1);
+    EXPECT_EQ(s.init(0,0),-1);
+
+    EXPECT_EQ(s.close(),0);
+    EXPECT_EQ(s.close(),-1);
+    EXPECT_EQ(s.close(),-1);
+}
