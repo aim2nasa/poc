b20dd65b2ab240588b8f4678f8395a5238634c8d
Rossi <rossi.kwak@gmail.com>
2019-04-24 17:11:52 +0900

    Change the definition of class Detect to be derived from IReceive interface. When recv is called back, data received is inserted into the queue of Detect class

--- test/wc2/src/Detect.h
+++ test/wc2/src/Detect.h
@@ -2,16 +2,24 @@
 #define __DETECT_H__
 
 #include "SafeQueue.h"
+#include "ISession.h"
 typedef unsigned char byte;
 typedef SafeQueue<std::vector<byte>> Queue;
 
-class Detect{
+class Detect : public IReceive{
 public:
     Detect(){}
     virtual ~Detect(){}
 
     int start(void *arg) { return pthread_create(&tid_,NULL,run,arg); }
     int join(void **retval=0) { return pthread_join(tid_,retval); }
+    ssize_t recv(void *buf,size_t len)
+    {
+        std::vector<byte> msg(len);
+        memcpy(msg.data(),buf,len);
+        q_.enqueue(msg);
+        return len;
+    }
 
 private:
     pthread_t tid_;
