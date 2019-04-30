41af2c55b376f92c5315090ed914a02c77a60714
Rossi Kwak <rossi.kwak@gmail.com>
2019-04-30 12:41:07 +0900

    add string size test

--- test/gtest/base.cpp
+++ test/gtest/base.cpp
@@ -3,3 +3,14 @@
 TEST(SimpleTest, comparison) { 
 	ASSERT_EQ(0, 0);
 }
+
+TEST(StringTest, size) {
+    std::string str("abc");
+    ASSERT_EQ(str.size(),3);
+
+    char tmp[]={0x41,0x42,0x43,0,0x45};
+    ASSERT_EQ(sizeof(tmp),5);
+
+    std::string tmpStr(tmp);
+    ASSERT_EQ(tmpStr.size(),3); //because of 0 in the 4th
+}
