ecd53cf7b3226b7f5e37fc252ec711ef1728648f
Rossi Kwak <rossi.kwak@gmail.com>
2019-04-30 13:31:57 +0900

    test on CryptoPP::StringSource and CryptoPP::StringSink

--- test/gtest/base.cpp
+++ test/gtest/base.cpp
@@ -1,4 +1,6 @@
 #include <gtest/gtest.h>
+#include <cryptopp/filters.h>
+#include <cryptopp/hex.h>
  
 TEST(SimpleTest, comparison) { 
 	ASSERT_EQ(0, 0);
@@ -13,4 +15,20 @@ TEST(StringTest, size) {
 
     std::string tmpStr(tmp);
     ASSERT_EQ(tmpStr.size(),3); //because of 0 in the 4th
+}
+
+using namespace CryptoPP;
+
+TEST(StringTest, stringSourceSink)
+{
+    char abc[]={0x41,0x42,0x43,0x44,0x45};
+    std::string str(abc);
+    std::string encoded;
+
+    StringSource(str,true,
+        new HexEncoder(
+            new StringSink(encoded)
+        )
+    );
+    ASSERT_EQ("4142434445",encoded);
 }
