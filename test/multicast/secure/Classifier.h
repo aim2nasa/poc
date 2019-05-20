55916b39914c1fd552c20b3cc93b3ed362f05b03
Rossi Kwak <rossi.kwak@gmail.com>
2019-05-20 14:21:53 +0900

    extract classifying function into Classifier class from FraudDetect and reflect changes to related codes

---
+++ test/multicast/secure/Classifier.h
@@ -0,0 +1,52 @@
+#ifndef __CLASSIFIER_H__
+#define __CLASSIFIER_H__
+
+#include <vector>
+#include "messageCount.h"
+#include <entity/node.h>
+
+#define MAX_QUEUE 5
+
+struct vcRtn{
+    vcRtn():visitCount(-1),order(-1){}
+    int visitCount;
+    int order;
+};
+
+class IDetect;
+
+class Classifier{
+public:
+	Classifier();
+	~Classifier();
+
+    enum kind{
+        initErr=-2,
+        unexpected=-1,
+        verified=0,
+        sequence=1,
+        replay=2,
+        fraud=3,
+        unAuthPub=4
+    };
+
+    void init(int tagSize,std::string adata,int keySize,int key,int iv,IDetect *p=NULL);
+    int ask(const char* buf, size_t size);
+    std::vector<messageCount> q_;
+
+private:
+    void setKeys(int size,int key,int iv);
+    void setDetect(IDetect *p);
+    vcRtn getVisitCount(std::vector<messageCount>& q,const char *buff,unsigned int buffSize);
+    int getFrameNumDiff(unsigned int frameNumber);
+
+    int tagSize_;
+    std::string adata_;
+    CryptoPP::GCM<CryptoPP::AES>::Decryption d_;
+    Node Bob_;
+    IDetect *detect_;
+    bool prevFrameDefined_;
+    unsigned int prevFrameNumber_;
+};
+
+#endif
--- test/multicast/secure/FraudDetect.h
+++ test/multicast/secure/FraudDetect.h
@@ -1,19 +1,7 @@
 #ifndef __FRAUDDETECT_H__
 #define __FRAUDDETECT_H__
 
-#include <vector>
-#include "messageCount.h"
-#include <entity/node.h>
-
-#define MAX_QUEUE 5
-
-struct vcRtn{
-    vcRtn():visitCount(-1),order(-1){}
-    int visitCount;
-    int order;
-};
-
-class IDetect;
+#include "Classifier.h"
 
 class FraudDetect{
 public:
@@ -23,20 +11,13 @@ public:
     int init(int port,int backlog=0);
     int start(void *arg);
     static void* run(void *arg);
-    void setKeys(int size,int key,int iv);
-    int getFrameNumDiff(unsigned int frameNumber);
     static void verbosity(bool b);
-    void setDetect(IDetect *p);
 
     int msqid_;
+    Classifier cf_;
 protected:
-    static vcRtn getVisitCount(std::vector<messageCount>& q,const char *buff,unsigned int buffSize);
     int sock_;
-    Node Bob_;
-    bool prevFrameDefined_;
-    unsigned int prevFrameNumber_;
     static bool verbosity_;
-    IDetect *detect_;
 };
 
 #endif
