24b38c72bbdbdad23294e2cb66675d2e5cb02eb4
Rossi Kwak <rossi.kwak@gmail.com>
2019-10-30 13:06:17 +0900

    bug fix, broken inclusion fixed

--- test/multicast/secure/replay.cpp
+++ test/multicast/secure/replay.cpp
@@ -8,7 +8,7 @@
 #include <stdlib.h>
 #include <pthread.h>
 #include <unistd.h>
-#include <../../wc2/src/SafeQueue.h>
+#include <../../test/wc2/src/SafeQueue.h>
 
 #define MULTICAST_GROUP "225.0.0.37"
 #define MULTICAST_PORT 12345
